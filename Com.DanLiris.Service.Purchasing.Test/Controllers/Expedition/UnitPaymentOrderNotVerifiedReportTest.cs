55d0014a38f7eef39b3119e175794c01e608f267
achensetyawati <achenzetya@gmail.com>
2018-05-23 09:00:29 +0700

    fix unit test

--- Com.DanLiris.Service.Purchasing.Test/Controllers/Expedition/UnitPaymentOrderNotVerifiedReportTest.cs
+++ Com.DanLiris.Service.Purchasing.Test/Controllers/Expedition/UnitPaymentOrderNotVerifiedReportTest.cs
@@ -1,10 +1,46 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace Com.DanLiris.Service.Purchasing.Test.Controllers.Expedition
 {
-    class UnitPaymentOrderNotVerifiedReportTest
+    [Collection("TestServerFixture Collection")]
+    public class UnitPaymentOrderNotVerifiedReportTest
     {
+        private const string MediaType = "application/json";
+        private readonly string URI = "v1/expedition/purchasing/unit-payment-orders-not-verified-report";
+        private readonly List<string> CreateValidationAttributes;
+
+        private TestServerFixture TestFixture { get; set; }
+
+        private HttpClient Client
+        {
+            get { return this.TestFixture.Client; }
+        }
+
+        public UnitPaymentOrderNotVerifiedReportTest(TestServerFixture fixture)
+        {
+            TestFixture = fixture;
+        }
+
+        [Fact]
+        public async Task Should_Success_Get_All_Data()
+        {
+            var response = await this.Client.GetAsync(URI);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var json = response.Content.ReadAsStringAsync().Result;
+            Dictionary<string, object> result = JsonConvert.DeserializeObject<Dictionary<string, object>>(json.ToString());
+
+            Assert.True(result.ContainsKey("apiVersion"));
+            Assert.True(result.ContainsKey("message"));
+            Assert.True(result.ContainsKey("data"));
+            Assert.True(result["data"].GetType().Name.Equals("JArray"));
+        }
     }
 }
