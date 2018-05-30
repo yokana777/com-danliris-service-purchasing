2a61be9c99d39bd48b864841cc92fc0c58c0031c
kuswandanu <kuswandanu@gmail.com>
2018-05-30 10:31:16 +0700

    validation

--- Com.DanLiris.Service.Purchasing.Lib/ViewModels/PurchaseRequestViewModel/PurchaseRequestViewModel.cs
+++ Com.DanLiris.Service.Purchasing.Lib/ViewModels/PurchaseRequestViewModel/PurchaseRequestViewModel.cs
@@ -3,6 +3,7 @@ using Com.DanLiris.Service.Purchasing.Lib.ViewModels.IntegrationViewModel;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Text;
 
 namespace Com.DanLiris.Service.Purchasing.Lib.ViewModels.PurchaseRequestViewModel
@@ -10,8 +11,8 @@ namespace Com.DanLiris.Service.Purchasing.Lib.ViewModels.PurchaseRequestViewMode
     public class PurchaseRequestViewModel : BaseViewModel, IValidatableObject
     {
         public string no { get; set; }
-        public DateTimeOffset date { get; set; }
-        public DateTimeOffset expectedDeliveryDate { get; set; }
+        public DateTimeOffset? date { get; set; }
+        public DateTimeOffset? expectedDeliveryDate { get; set; }
         public BudgetViewModel budget { get; set; }
         public UnitViewModel unit { get; set; }
         public CategoryViewModel category { get; set; }
@@ -23,9 +24,13 @@ namespace Com.DanLiris.Service.Purchasing.Lib.ViewModels.PurchaseRequestViewMode
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            if (this.date.Equals(DateTimeOffset.MinValue))
+            if (this.date.Equals(DateTimeOffset.MinValue) || this.date == null)
             {
                 yield return new ValidationResult("Date is required", new List<string> { "date" });
+            }
+            else if (this.expectedDeliveryDate != null && this.date > this.expectedDeliveryDate)
+            {
+                yield return new ValidationResult("Date is greater than expected delivery date", new List<string> { "date" });
             }
             if (this.budget == null)
             {
@@ -40,9 +45,48 @@ namespace Com.DanLiris.Service.Purchasing.Lib.ViewModels.PurchaseRequestViewMode
                 yield return new ValidationResult("Category is required", new List<string> { "category" });
             }
 
+            int itemErrorCount = 0;
+
             if (this.items.Count.Equals(0))
             {
                 yield return new ValidationResult("Items is required", new List<string> { "itemscount" });
+            }
+            else
+            {
+                string itemError = "[";
+
+                foreach (PurchaseRequestItemViewModel item in items)
+                {
+                    itemError += "{";
+
+                    if (item.product == null || string.IsNullOrWhiteSpace(item.product._id))
+                    {
+                        itemErrorCount++;
+                        itemError += "product: 'Product is required', ";
+                    }
+                    else
+                    {
+                        var itemsExist = items.Where(i => i.product != null && item.product != null && i.product._id.Equals(item.product._id)).Count();
+                        if (itemsExist > 1)
+                        {
+                            itemErrorCount++;
+                            itemError += "product: 'Product is duplicate', ";
+                        }
+                    }
+
+                    if (item.quantity <= 0)
+                    {
+                        itemErrorCount++;
+                        itemError += "quantity: 'Quantity should be more than 0'";
+                    }
+
+                    itemError += "}, ";
+                }
+
+                itemError += "]";
+
+                if (itemErrorCount > 0)
+                    yield return new ValidationResult(itemError, new List<string> { "items" });
             }
         }
     }
