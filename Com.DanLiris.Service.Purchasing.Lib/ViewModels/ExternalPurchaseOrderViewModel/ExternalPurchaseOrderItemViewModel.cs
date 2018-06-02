cb90d3d413f6c5cc3a7749df9f43ef7ccbc5dbcf
achensetyawati <achenzetya@gmail.com>
2018-06-02 12:55:27 +0700

    merge conflict

---
+++ Com.DanLiris.Service.Purchasing.Lib/ViewModels/ExternalPurchaseOrderViewModel/ExternalPurchaseOrderItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.DanLiris.Service.Purchasing.Lib.ViewModels.ExternalPurchaseOrderViewModel
+{
+    class ExternalPurchaseOrderItemViewModel
+    {
+    }
+}
