502ebc71290490cd619d3c6101b2511ecfc844ab
jackyruslymoonlay <jacky.rusly@moonlay.com>
2018-07-03 11:21:49 +0700

    Add Payment Method, Date Expedition and PPH

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20180703042115_PaymentMethod_Expedition_DatePPH.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20180703042115_PaymentMethod_Expedition_DatePPH.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180703042115_PaymentMethod_Expedition_DatePPH")]
+    partial class PaymentMethod_Expedition_DatePPH
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
