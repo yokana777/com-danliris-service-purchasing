388e014e5268984ad1b56f9af2cbfb1c856607b4
leslie.aula <leslie.aula@moonlay.com>
2018-07-03 15:51:01 +0700

    Update Bank Expenditure Note Add Unit Test

--- Com.DanLiris.Service.Purchasing.Lib/ViewModels/BankExpenditureNote/BankExpenditureNoteViewModel.cs
+++ Com.DanLiris.Service.Purchasing.Lib/ViewModels/BankExpenditureNote/BankExpenditureNoteViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Com.DanLiris.Service.Purchasing.Lib.Utilities;
 using Com.DanLiris.Service.Purchasing.Lib.ViewModels.IntegrationViewModel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -9,6 +10,7 @@ namespace Com.DanLiris.Service.Purchasing.Lib.ViewModels.BankExpenditureNote
     {
         public AccountBankViewModel Bank { get; set; }
         public string BGCheckNumber { get; set; }
+        public DateTimeOffset Date { get; set; }
         public List<BankExpenditureNoteDetailViewModel> Details { get; set; }
         public string DocumentNo { get; set; }
         public SupplierViewModel Supplier { get; set; }
@@ -20,20 +22,20 @@ namespace Com.DanLiris.Service.Purchasing.Lib.ViewModels.BankExpenditureNote
                 yield return new ValidationResult("Minimal 1 Surat Perintah Bayar", new List<string> { "Details" });
             }
 
-            if (Supplier == null)
-            {
-                yield return new ValidationResult("Supplier harus diisi", new List<string> { "Supplier" });
-            }
-
             if (Bank == null)
             {
                 yield return new ValidationResult("Bank harus diisi", new List<string> { "Bank" });
+            }
+
+            if (Date == null)
+            {
+                yield return new ValidationResult("Tanggal harus diisi", new List<string> { "Date" });
             }
 
             if (string.IsNullOrWhiteSpace(BGCheckNumber))
             {
                 yield return new ValidationResult("Cek/BG harus diisi", new List<string> { "BGCheckNumber" });
             }
-        }            
+        }
     }
 }
--- Com.DanLiris.Service.Purchasing.Lib/ViewModels/IntegrationViewModel/BankViewModel.cs
+++ Com.DanLiris.Service.Purchasing.Lib/ViewModels/IntegrationViewModel/BankViewModel.cs
@@ -9,6 +9,7 @@ namespace Com.DanLiris.Service.Purchasing.Lib.ViewModels.IntegrationViewModel
         public string _id { get; set; }
         public string code { get; set; }
         public string bankName { get; set; }
+        public string bankCode { get; set; }
         public string accountName { get; set; }
         public string accountNumber { get; set; }
         public CurrencyViewModel currency { get; set; }
