b2df61a6352145da288b570d305555dfc1b0b019
kuswandanu <kuswandanu@gmail.com>
2018-07-06 15:54:39 +0700

    Edit UnitPaymentOrder IncomeTaxDate Nullable

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20180705073525_UnitPaymentOrder_IncomeTaxDate_Nullable.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20180705073525_UnitPaymentOrder_IncomeTaxDate_Nullable.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180705073525_UnitPaymentOrder_IncomeTaxDate_Nullable")]
+    partial class UnitPaymentOrder_IncomeTaxDate_Nullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
