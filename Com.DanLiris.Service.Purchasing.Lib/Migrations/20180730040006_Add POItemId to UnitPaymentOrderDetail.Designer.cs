65b41b2c5eb4b2cb6db464b474e5ced3d31e6dcf
kuswandanu <kuswandanu@gmail.com>
2018-07-30 16:15:16 +0700

    Add POItemId to UnitPaymentOrderDetail

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20180730040006_Add POItemId to UnitPaymentOrderDetail.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20180730040006_Add POItemId to UnitPaymentOrderDetail.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180730040006_Add POItemId to UnitPaymentOrderDetail")]
+    partial class AddPOItemIdtoUnitPaymentOrderDetail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
