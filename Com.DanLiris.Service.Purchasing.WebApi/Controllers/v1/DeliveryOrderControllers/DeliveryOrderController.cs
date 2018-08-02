08ec2cdc89db0d6232c666e434b36535b1163f73
kharissuryandaru <35208779+kharissuryandaru@users.noreply.github.com>
2018-08-02 13:51:58 +0700

    Fixed Monitoring SJ

--- Com.DanLiris.Service.Purchasing.WebApi/Controllers/v1/DeliveryOrderControllers/DeliveryOrderController.cs
+++ Com.DanLiris.Service.Purchasing.WebApi/Controllers/v1/DeliveryOrderControllers/DeliveryOrderController.cs
@@ -247,5 +247,66 @@ namespace Com.DanLiris.Service.Purchasing.WebApi.Controllers.v1.DeliveryOrderCon
                 },
             });
         }
+
+        //MONITORING
+        #region MONITORING
+        [HttpGet("monitoring")]
+        public IActionResult GetReport(string doNo, string supplierId, DateTime? dateFrom, DateTime? dateTo, int page, int size, string Order = "{}")
+        {
+            int offset = Convert.ToInt32(Request.Headers["x-timezone-offset"]);
+            string accept = Request.Headers["Accept"];
+
+            try
+            {
+
+                var data = facade.GetReport(doNo, supplierId, dateFrom, dateTo, page, size, Order, offset);
+
+                return Ok(new
+                {
+                    apiVersion = ApiVersion,
+                    data = data.Item1,
+                    info = new { total = data.Item2 },
+                    message = General.OK_MESSAGE,
+                    statusCode = General.OK_STATUS_CODE
+                });
+            }
+            catch (Exception e)
+            {
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.INTERNAL_ERROR_STATUS_CODE, e.Message)
+                    .Fail();
+                return StatusCode(General.INTERNAL_ERROR_STATUS_CODE, Result);
+            }
+        }
+
+        [HttpGet("monitoring/download")]
+        public IActionResult GetXls(string doNo, string supplierId, DateTime? dateFrom, DateTime? dateTo)
+        {
+
+            try
+            {
+                byte[] xlsInBytes;
+                int offset = Convert.ToInt32(Request.Headers["x-timezone-offset"]);
+                DateTime DateFrom = dateFrom == null ? new DateTime(1970, 1, 1) : Convert.ToDateTime(dateFrom);
+                DateTime DateTo = dateTo == null ? DateTime.Now : Convert.ToDateTime(dateTo);
+
+                var xls = facade.GenerateExcel(doNo, supplierId, dateFrom, dateTo, offset);
+
+                string filename = String.Format("Surat Jalan - {0}.xlsx", DateTime.UtcNow.ToString("ddMMyyyy"));
+
+                xlsInBytes = xls.ToArray();
+                var file = File(xlsInBytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", filename);
+                return file;
+
+            }
+            catch (Exception e)
+            {
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.INTERNAL_ERROR_STATUS_CODE, e.Message)
+                    .Fail();
+                return StatusCode(General.INTERNAL_ERROR_STATUS_CODE, Result);
+            }
+        }
+        #endregion
     }
 }
