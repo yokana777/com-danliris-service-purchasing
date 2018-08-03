5b56f034838c1418ad19bdd556b221d48118ffd6
achensetyawati <achenzetya@gmail.com>
2018-08-03 14:18:29 +0700

    update SPB (migration)

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20180803070400_addQuantityCorrectionSPB.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20180803070400_addQuantityCorrectionSPB.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180803070400_addQuantityCorrectionSPB")]
+    partial class addQuantityCorrectionSPB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
