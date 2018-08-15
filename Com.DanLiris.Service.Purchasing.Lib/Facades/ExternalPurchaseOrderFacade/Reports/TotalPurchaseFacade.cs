4137646e9d6c0b2df9f044d529a8f5d7715333fc
mocia <mo_cia@yahoo.com>
2018-08-15 12:40:37 +0700

    update unit test excell

--- Com.DanLiris.Service.Purchasing.Lib/Facades/ExternalPurchaseOrderFacade/Reports/TotalPurchaseFacade.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Facades/ExternalPurchaseOrderFacade/Reports/TotalPurchaseFacade.cs
@@ -82,7 +82,7 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.ExternalPurchaseOrderFacad
 
 			decimal Total = 0;
 			if (Query.ToArray().Count() == 0)
-				result.Rows.Add("", "", "", "", "", ""); // to allow column name to be generated properly for empty data as template
+				result.Rows.Add("", "", "", "",0 ,0); // to allow column name to be generated properly for empty data as template
 			else
 			{
 				int index = 0;
