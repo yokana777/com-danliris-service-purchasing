e7e76625c6048860be62a01c2503577b1361d2c9
achensetyawati <achenzetya@gmail.com>
2018-08-25 12:44:13 +0700

    fix test

--- Com.DanLiris.Service.Purchasing.Test/Facades/InternalPurchaseOrderTests/POMonitoringAllTest.cs
+++ Com.DanLiris.Service.Purchasing.Test/Facades/InternalPurchaseOrderTests/POMonitoringAllTest.cs
@@ -1,7 +1,9 @@
 ﻿using Com.DanLiris.Service.Purchasing.Lib.Facades.InternalPO;
 using Com.DanLiris.Service.Purchasing.Lib.Models.InternalPurchaseOrderModel;
+using Com.DanLiris.Service.Purchasing.Lib.Models.UnitPaymentCorrectionNoteModel;
 using Com.DanLiris.Service.Purchasing.Lib.Services;
 using Com.DanLiris.Service.Purchasing.Test.DataUtils.InternalPurchaseOrderDataUtils;
+using Com.DanLiris.Service.Purchasing.Test.DataUtils.UnitPaymentCorrectionNoteDataUtils;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -27,6 +29,11 @@ namespace Com.DanLiris.Service.Purchasing.Test.Facades.InternalPurchaseOrderTest
             get { return (InternalPurchaseOrderDataUtil)ServiceProvider.GetService(typeof(InternalPurchaseOrderDataUtil)); }
         }
 
+        private UnitPaymentCorrectionNoteDataUtil DataCorrectionUtil
+        {
+            get { return (UnitPaymentCorrectionNoteDataUtil)ServiceProvider.GetService(typeof(UnitPaymentCorrectionNoteDataUtil)); }
+        }
+
         private PurchaseOrderMonitoringAllFacade Facade
         {
             get { return (PurchaseOrderMonitoringAllFacade)ServiceProvider.GetService(typeof(PurchaseOrderMonitoringAllFacade)); }
@@ -38,6 +45,14 @@ namespace Com.DanLiris.Service.Purchasing.Test.Facades.InternalPurchaseOrderTest
             InternalPurchaseOrder model = await DataUtil.GetTestData("Unit test");
             var Response = Facade.GetReport(model.PRNo, null, model.UnitId, model.CategoryId, null , null ,model.CreatedBy, null, null,null, 1, 25, "{}", 7,"");
             Assert.NotNull(Response);
+        }
+
+        [Fact]
+        public async void Should_Success_Get_Report_Data_All_Null_Parameter()
+        {
+            UnitPaymentCorrectionNote model = await DataCorrectionUtil.GetTestData();
+            var Response = Facade.GetReport("", null, null, null, null, null, null, null, null, null, 1, 25, "{}", 7, "");
+            Assert.NotEqual(Response.Item2, 0);
         }
 
         [Fact]
