1c2538c17b9fcb4c9fd9a803c7ef7c16c81ea45a
kharissuryandaru <35208779+kharissuryandaru@users.noreply.github.com>
2018-08-27 13:42:10 +0700

    Fixed LocalPurchasingBookReport

--- Com.DanLiris.Service.Purchasing.Lib/ViewModels/UnitReceiptNote/LocalPurchasingBookReportViewModel.cs
+++ Com.DanLiris.Service.Purchasing.Lib/ViewModels/UnitReceiptNote/LocalPurchasingBookReportViewModel.cs
@@ -7,14 +7,14 @@ namespace Com.DanLiris.Service.Purchasing.Lib.ViewModels.UnitReceiptNote
 {
     public class LocalPurchasingBookReportViewModel : BaseViewModel
     {
-        public DateTimeOffset receiptDate { get; set; }
+        public string receiptDate { get; set; }
         public string uRNNo { get; set; }
         public string productName { get; set; }
         public string invoiceNo { get; set; }
         public string categoryName { get; set; }
         public string unitName { get; set; }
-        public string dpp { get; set; }
-        public string ppn { get; set; }
-        public string total { get; set; }
+        public decimal dpp { get; set; }
+        public decimal ppn { get; set; }
+        public bool useVat { get; set; }
     }
 }
