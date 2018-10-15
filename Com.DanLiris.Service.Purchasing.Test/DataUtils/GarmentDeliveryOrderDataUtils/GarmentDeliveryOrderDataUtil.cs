9a363e7de95beb36e9246fcd6719aa15c8544cd5
kharissuryandaru <35208779+kharissuryandaru@users.noreply.github.com>
2018-10-15 08:35:42 +0700

    Fixed Garment Delivery Order

---
+++ Com.DanLiris.Service.Purchasing.Test/DataUtils/GarmentDeliveryOrderDataUtils/GarmentDeliveryOrderDataUtil.cs
@@ -0,0 +1,54 @@
+﻿//using Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentDeliveryOrderFacades;
+//using Com.DanLiris.Service.Purchasing.Lib.Models.GarmentDeliveryOrderModel;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Com.DanLiris.Service.Purchasing.Test.DataUtils.GarmentDeliveryOrderDataUtils
+//{
+//    public class GarmentDeliveryOrderDataUtil
+//    {
+//        private readonly GarmentDeliveryOrderFacade facade;
+
+//        public GarmentDeliveryOrderDataUtil(GarmentDeliveryOrderFacade facade)
+//        {
+//            this.facade = facade;
+//        }
+
+//        public GarmentDeliveryOrder GetNewData()
+//        {
+//            Random rnd = new Random();
+//            long nowTicks = DateTimeOffset.Now.Ticks;
+//            string nowTicksA = $"{nowTicks}a";
+//            string nowTicksB = $"{nowTicks}b";
+
+//            return new GarmentDeliveryOrder
+//            {
+//                DONo = $"{nowTicksA}",
+
+//                SupplierId = $"{nowTicksA}",
+//                SupplierCode = $"BuyerCode{nowTicksA}",
+//                SupplierName = $"BuyerName{nowTicksA}",
+
+//                DODate = DateTimeOffset.Now,
+//                ArrivalDate = DateTimeOffset.Now,
+
+//                ShipmentType = $"ShipmentType{nowTicksA}",
+//                ShipmentNo = $"ShipmentNo{nowTicksA}",
+
+//                Remark = $"Remark{nowTicksA}",
+
+//                IsClosed = false,
+//                IsCostum = false,
+//            };
+//        }
+
+//        public async Task<GarmentDeliveryOrder> GetTestData()
+//        {
+//            var data = GetNewData();
+//            await facade.Create(data, "Unit Test");
+//            return data;
+//        }
+//    }
+//}
