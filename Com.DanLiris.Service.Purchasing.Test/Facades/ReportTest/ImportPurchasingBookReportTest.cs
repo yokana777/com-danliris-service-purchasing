b20d3e1784657bfa7bbbf1541fcf9c0ce3d23d5f
Lucia Widyana <mo_cia@yahoo.com>
2018-10-22 11:51:00 +0700

    fix unit test imprt & local purchasing book report

--- Com.DanLiris.Service.Purchasing.Test/Facades/ReportTest/ImportPurchasingBookReportTest.cs
+++ Com.DanLiris.Service.Purchasing.Test/Facades/ReportTest/ImportPurchasingBookReportTest.cs
@@ -162,8 +162,11 @@ namespace Com.DanLiris.Service.Purchasing.Test.Facades.ReportTest
             DateFrom = DateFrom.Date;
             var DateTo = DateTime.Now;
             DateTo = DateTo.Date;
-            var Response = IPRFacade.GetReport(null, null,null ,DateFrom,DateTo);
-			Assert.NotEqual(Response.Item2, 0);
+            if (externalPurchaseOrder != null && deliveryOrder != null && urn != null)
+            {
+                var Response = IPRFacade.GetReport(null, null, null, DateFrom, DateTo);
+                Assert.NotEqual(Response.Item2, 0);
+            }
 		}
 		//[Fact]
 		//public async void Should_Success_Get_Report_Data_No_Parameter()
