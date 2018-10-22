3be08cc4b6e82a917b10f149bdca190b97d0a915
achensetyawati <achenzetya@gmail.com>
2018-10-22 09:44:47 +0700

    Update Migrations

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20181022012652_Garment_EPO_OverBudgetRemark.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20181022012652_Garment_EPO_OverBudgetRemark.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181022012652_Garment_EPO_OverBudgetRemark")]
+    partial class Garment_EPO_OverBudgetRemark
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
