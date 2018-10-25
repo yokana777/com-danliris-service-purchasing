fd8702bcbd35d769382879b49e969d69f7d9375e
kharissuryandaru <35208779+kharissuryandaru@users.noreply.github.com>
2018-10-25 09:47:48 +0700

    Fixed Test Local & Import PurchasingBookReport

--- Com.DanLiris.Service.Purchasing.Test/Facades/ReportTest/ImportPurchasingBookReportTest.cs
+++ Com.DanLiris.Service.Purchasing.Test/Facades/ReportTest/ImportPurchasingBookReportTest.cs
@@ -147,75 +147,84 @@ namespace Com.DanLiris.Service.Purchasing.Test.Facades.ReportTest
         //}
 
         [Fact]
-		public async void Should_Success_Get_Report_Data()
-		{
-			ExternalPurchaseOrder externalPurchaseOrder = await EPODataUtil.GetNewData("unit-test");
-			await EPOFacade.Create(externalPurchaseOrder, "unit-test", 7);
-			DeliveryOrder deliveryOrder = await DODataUtil.GetNewData("unit-test");
-			await DOFacade.Create(deliveryOrder, "unit-test");
-			UnitReceiptNote urn = await DataUtil.GetNewDatas("unit-test");
-			await Facade.Create(urn, "unit-test");
-            //UnitPaymentOrder upo = await UPODataUtil.GetTestData();
-            //await UPOFacade.Create(upo, "unit-test", false, 7);
+        //public async void Should_Success_Get_Report_Data()
+        //{
+        //	ExternalPurchaseOrder externalPurchaseOrder = await EPODataUtil.GetNewData("unit-test");
+        //	await EPOFacade.Create(externalPurchaseOrder, "unit-test", 7);
+        //	DeliveryOrder deliveryOrder = await DODataUtil.GetNewData("unit-test");
+        //	await DOFacade.Create(deliveryOrder, "unit-test");
+        //	UnitReceiptNote urn = await DataUtil.GetNewDatas("unit-test");
+        //	await Facade.Create(urn, "unit-test");
+        //          //UnitPaymentOrder upo = await UPODataUtil.GetTestData();
+        //          //await UPOFacade.Create(upo, "unit-test", false, 7);
+        //          var DateFrom = DateTime.Now;
+        //          DateFrom = DateFrom.Date;
+        //          var DateTo = DateTime.Now;
+        //          DateTo = DateTo.Date;
+        //          var Response = IPRFacade.GetReport(null, null,null ,DateFrom,DateTo);
+        //	Assert.NotEqual(Response.Item2, 0);
+        //}
+        public async void Should_Success_Get_Report_Data()
+        {
+            ExternalPurchaseOrder externalPurchaseOrder = await EPODataUtil.GetTestData("Unit test");
+            DeliveryOrder deliveryOrder = await DODataUtil.GetTestData("unit-test");
+            UnitReceiptNote urn = await DataUtil.GetTestData2("unit-test");
             var DateFrom = DateTime.Now;
             DateFrom = DateFrom.Date;
             var DateTo = DateTime.Now;
             DateTo = DateTo.Date;
-            var Response = IPRFacade.GetReport(null, null,null ,DateFrom,DateTo);
-			Assert.NotEqual(Response.Item2, 0);
-		}
-		//[Fact]
-		//public async void Should_Success_Get_Report_Data_No_Parameter()
-		//{
-		//	ExternalPurchaseOrder externalPurchaseOrder = await EPODataUtil.GetNewData("unit-test");
-		//	await EPOFacade.Create(externalPurchaseOrder, "unit-test", 7);
-		//	DeliveryOrder deliveryOrder = await DODataUtil.GetNewData("unit-test");
-		//	await DOFacade.Create(deliveryOrder, "unit-test");
-		//	UnitReceiptNote urn = await DataUtil.GetNewDatas("unit-test");
-		//	await Facade.Create(urn, "unit-test");
-		//	DateTime DateFrom = new DateTime(2018, 8, 27);
-		//	DateTime DateTo = new DateTime(2018, 8, 27);
-		//	var Response = IPRFacade.GetReport("18-08-BPI-001-unitcode-001", null, null, DateFrom, DateTo);
-		//	Assert.NotEqual(Response.Item2, 0);
-		//}
-		//[Fact]
-		//public async void Should_Success_Get_Report_Data_Unit_Parameter()
-		//{
-		//	ExternalPurchaseOrder externalPurchaseOrder = await EPODataUtil.GetNewData("unit-test");
-		//	await EPOFacade.Create(externalPurchaseOrder, "unit-test", 7);
-		//	DeliveryOrder deliveryOrder = await DODataUtil.GetNewData("unit-test");
-		//	await DOFacade.Create(deliveryOrder, "unit-test");
-		//	UnitReceiptNote urn = await DataUtil.GetNewDatas("unit-test");
-		//	await Facade.Create(urn, "unit-test");
-		//	DateTime DateFrom = new DateTime(2018, 8, 27);
-		//	DateTime DateTo = new DateTime(2018, 8, 27);
-		//	var Response = IPRFacade.GetReport(null, "UnitName", null, DateFrom, DateTo);
-		//	Assert.NotEqual(Response.Item2, 0);
-		//}
-		//[Fact]
-		//public async void Should_Success_Get_Report_Data_Category_Parameter()
-		//{
-		//	ExternalPurchaseOrder externalPurchaseOrder = await EPODataUtil.GetNewData("unit-test");
-		//	await EPOFacade.Create(externalPurchaseOrder, "unit-test", 7);
-		//	DeliveryOrder deliveryOrder = await DODataUtil.GetNewData("unit-test");
-		//	await DOFacade.Create(deliveryOrder, "unit-test");
-		//	UnitReceiptNote urn = await DataUtil.GetNewDatas("unit-test");
-		//	await Facade.Create(urn, "unit-test");
-		//	DateTime DateFrom = new DateTime(2018, 8, 27);
-		//	DateTime DateTo = new DateTime(2018, 8, 27);
-		//	var Response = IPRFacade.GetReport(null, null, "CategoryName", DateFrom, DateTo);
-		//	Assert.NotEqual(Response.Item2, 0);
-		//}
+            var Response = IPRFacade.GetReport(null, null, null, DateFrom, DateTo);
+            Assert.NotEqual(Response.Item2, 0);
+        }
+        //[Fact]
+        //public async void Should_Success_Get_Report_Data_No_Parameter()
+        //{
+        //	ExternalPurchaseOrder externalPurchaseOrder = await EPODataUtil.GetNewData("unit-test");
+        //	await EPOFacade.Create(externalPurchaseOrder, "unit-test", 7);
+        //	DeliveryOrder deliveryOrder = await DODataUtil.GetNewData("unit-test");
+        //	await DOFacade.Create(deliveryOrder, "unit-test");
+        //	UnitReceiptNote urn = await DataUtil.GetNewDatas("unit-test");
+        //	await Facade.Create(urn, "unit-test");
+        //	DateTime DateFrom = new DateTime(2018, 8, 27);
+        //	DateTime DateTo = new DateTime(2018, 8, 27);
+        //	var Response = IPRFacade.GetReport("18-08-BPI-001-unitcode-001", null, null, DateFrom, DateTo);
+        //	Assert.NotEqual(Response.Item2, 0);
+        //}
+        //[Fact]
+        //public async void Should_Success_Get_Report_Data_Unit_Parameter()
+        //{
+        //	ExternalPurchaseOrder externalPurchaseOrder = await EPODataUtil.GetNewData("unit-test");
+        //	await EPOFacade.Create(externalPurchaseOrder, "unit-test", 7);
+        //	DeliveryOrder deliveryOrder = await DODataUtil.GetNewData("unit-test");
+        //	await DOFacade.Create(deliveryOrder, "unit-test");
+        //	UnitReceiptNote urn = await DataUtil.GetNewDatas("unit-test");
+        //	await Facade.Create(urn, "unit-test");
+        //	DateTime DateFrom = new DateTime(2018, 8, 27);
+        //	DateTime DateTo = new DateTime(2018, 8, 27);
+        //	var Response = IPRFacade.GetReport(null, "UnitName", null, DateFrom, DateTo);
+        //	Assert.NotEqual(Response.Item2, 0);
+        //}
+        //[Fact]
+        //public async void Should_Success_Get_Report_Data_Category_Parameter()
+        //{
+        //	ExternalPurchaseOrder externalPurchaseOrder = await EPODataUtil.GetNewData("unit-test");
+        //	await EPOFacade.Create(externalPurchaseOrder, "unit-test", 7);
+        //	DeliveryOrder deliveryOrder = await DODataUtil.GetNewData("unit-test");
+        //	await DOFacade.Create(deliveryOrder, "unit-test");
+        //	UnitReceiptNote urn = await DataUtil.GetNewDatas("unit-test");
+        //	await Facade.Create(urn, "unit-test");
+        //	DateTime DateFrom = new DateTime(2018, 8, 27);
+        //	DateTime DateTo = new DateTime(2018, 8, 27);
+        //	var Response = IPRFacade.GetReport(null, null, "CategoryName", DateFrom, DateTo);
+        //	Assert.NotEqual(Response.Item2, 0);
+        //}
 
-		[Fact]
+        [Fact]
 		public async void Should_Success_Get_Report_Data_Excel_Null_Parameter()
 		{
-			ExternalPurchaseOrder externalPurchaseOrder = await EPODataUtil.GetNewData("unit-test");
-			await EPOFacade.Create(externalPurchaseOrder, "unit-test", 7);
-			DeliveryOrder deliveryOrder = await DODataUtil.GetNewData("unit-test");
-			await DOFacade.Create(deliveryOrder, "unit-test");
-			UnitReceiptNote urn = await DataUtil.GetNewDatas("unit-test");
-			await Facade.Create(urn, "unit-test");
+            ExternalPurchaseOrder externalPurchaseOrder = await EPODataUtil.GetTestData("Unit test");
+            DeliveryOrder deliveryOrder = await DODataUtil.GetTestData("unit-test");
+            UnitReceiptNote urn = await DataUtil.GetTestData2("unit-test");
             var DateFrom = DateTime.Now;
             DateFrom = DateFrom.Date;
             var DateTo = DateTime.Now;
@@ -226,12 +235,9 @@ namespace Com.DanLiris.Service.Purchasing.Test.Facades.ReportTest
 		[Fact]
 		public async void Should_Success_Get_Report_Total_Purchase_By_Units_Null_Data_Excel()
 		{
-			ExternalPurchaseOrder externalPurchaseOrder = await EPODataUtil.GetNewData("unit-test");
-			await EPOFacade.Create(externalPurchaseOrder, "unit-test", 7);
-			DeliveryOrder deliveryOrder = await DODataUtil.GetNewData("unit-test");
-			await DOFacade.Create(deliveryOrder, "unit-test");
-			UnitReceiptNote urn = await DataUtil.GetNewDatas("unit-test");
-			await Facade.Create(urn, "unit-test");
+            ExternalPurchaseOrder externalPurchaseOrder = await EPODataUtil.GetTestData("Unit test");
+            DeliveryOrder deliveryOrder = await DODataUtil.GetTestData("unit-test");
+            UnitReceiptNote urn = await DataUtil.GetTestData2("unit-test");
             var DateFrom = DateTime.Now;
             DateFrom = DateFrom.Date;
             var DateTo = DateTime.Now;
