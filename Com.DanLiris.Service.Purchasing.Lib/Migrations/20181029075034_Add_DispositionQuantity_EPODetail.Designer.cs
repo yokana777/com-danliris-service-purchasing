bff2f1a53f040997f0ae6d4a2ac4f425d3d44f3f
kuswandanu <kuswandanu@gmail.com>
2018-10-29 14:58:37 +0700

    Add_DispositionQuantity_EPODetail

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20181029075034_Add_DispositionQuantity_EPODetail.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20181029075034_Add_DispositionQuantity_EPODetail.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181029075034_Add_DispositionQuantity_EPODetail")]
+    partial class Add_DispositionQuantity_EPODetail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
