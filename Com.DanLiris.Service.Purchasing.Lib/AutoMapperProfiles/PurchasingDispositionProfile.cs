4a03bb8d38cca15de7a98957a3d1fb6c9599266d
achensetyawati <achenzetya@gmail.com>
2018-11-10 12:16:39 +0700

    Purchasing Disposition

--- Com.DanLiris.Service.Purchasing.Lib/AutoMapperProfiles/PurchasingDispositionProfile.cs
+++ Com.DanLiris.Service.Purchasing.Lib/AutoMapperProfiles/PurchasingDispositionProfile.cs
@@ -15,12 +15,18 @@ namespace Com.DanLiris.Service.Purchasing.Lib.AutoMapperProfiles
                 .ForPath(d => d.Supplier.Id, opt => opt.MapFrom(s => s.SupplierId))
                 .ForPath(d => d.Supplier.Code, opt => opt.MapFrom(s => s.SupplierCode))
                 .ForPath(d => d.Supplier.Name, opt => opt.MapFrom(s => s.SupplierName))
+                .ForPath(d => d.Currency.Id, opt => opt.MapFrom(s => s.CurrencyId))
+                .ForPath(d => d.Currency.Code, opt => opt.MapFrom(s => s.CurrencyCode))
+                .ForPath(d => d.Currency.Rate, opt => opt.MapFrom(s => s.CurrencyRate))
                 .ReverseMap();
 
             CreateMap<PurchasingDispositionItem, PurchasingDispositionItemViewModel>()
                 .ForPath(d => d.Unit.Id, opt => opt.MapFrom(s => s.UnitId))
                 .ForPath(d => d.Unit.Code, opt => opt.MapFrom(s => s.UnitCode))
                 .ForPath(d => d.Unit.Name, opt => opt.MapFrom(s => s.UnitName))
+                .ForPath(d => d.Unit.Division.Id, opt => opt.MapFrom(s => s.DivisionId))
+                .ForPath(d => d.Unit.Division.Code, opt => opt.MapFrom(s => s.DivisionCode))
+                .ForPath(d => d.Unit.Division.Name, opt => opt.MapFrom(s => s.DivisionName))
                 .ForPath(d => d.IncomeTax.Id, opt => opt.MapFrom(s => s.IncomeTaxId))
                 .ForPath(d => d.IncomeTax.Name, opt => opt.MapFrom(s => s.IncomeTaxName))
                 .ForPath(d => d.IncomeTax.Rate, opt => opt.MapFrom(s => s.IncomeTaxRate))
