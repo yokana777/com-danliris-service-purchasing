41d771f535244bbe9f0c44f3f4661a00b6c551fa
widiartowahyu3 <43228909+widiartowahyu3@users.noreply.github.com>
2018-12-22 17:01:39 +0700

    update unit test

--- Com.DanLiris.Service.Purchasing.Lib/ViewModels/GarmentUnitDeliveryOrderViewModel/GarmentUnitDeliveryOrderItemViewModel.cs
+++ Com.DanLiris.Service.Purchasing.Lib/ViewModels/GarmentUnitDeliveryOrderViewModel/GarmentUnitDeliveryOrderItemViewModel.cs
@@ -8,25 +8,25 @@ namespace Com.DanLiris.Service.Purchasing.Lib.ViewModels.GarmentUnitDeliveryOrde
 {
     public class GarmentUnitDeliveryOrderItemViewModel : BaseViewModel
     {
-        public long URNId { get; set; }
-        public string URNNo { get; set; }
-        public long URNItemId { get; set; }
-        public long DODetailId { get; set; }
-        public long EPOItemId { get; set; }
-        public long POItemId { get; set; }
-        public long PRItemId { get; set; }
-        public string POSerialNumber { get; set; }
+        //public long URNId { get; set; }
+        //public string URNNo { get; set; }
+        //public long URNItemId { get; set; }
+        //public long DODetailId { get; set; }
+        //public long EPOItemId { get; set; }
+        //public long POItemId { get; set; }
+        //public long PRItemId { get; set; }
+        //public string POSerialNumber { get; set; }
 
-        /*Product*/
-        public GarmentProductViewModel Product { get; set; }
+        ///*Product*/
+        //public GarmentProductViewModel Product { get; set; }
 
-        public string RONo { get; set; }
-        public double Quantity { get; set; }
+        //public string RONo { get; set; }
+        //public double Quantity { get; set; }
 
-        /*UOM*/
-        public UomViewModel Uom { get; set; }
+        ///*UOM*/
+        //public UomViewModel Uom { get; set; }
 
-        public double PricePerDealUnit { get; set; }
-        public string FabricType { get; set; }
+        //public double PricePerDealUnit { get; set; }
+        //public string FabricType { get; set; }
     }
 }
