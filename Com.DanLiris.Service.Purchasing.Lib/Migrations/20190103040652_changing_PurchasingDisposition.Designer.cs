2efd30eae169e863214bcdf9162e6a238d2e049f
achensetyawati <achenzetya@gmail.com>
2019-01-04 09:21:43 +0700

    fix Purchasing disposition model

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20190103040652_changing_PurchasingDisposition.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20190103040652_changing_PurchasingDisposition.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190103040652_changing_PurchasingDisposition")]
+    partial class changing_PurchasingDisposition
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
