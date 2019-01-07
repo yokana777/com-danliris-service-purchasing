f9a9e3037e29f0bee190bbdf976902f1fbc52254
Wahyu Widiarto <43228909+widiartowahyu3@users.noreply.github.com>
2019-01-07 15:29:39 +0700

    GarmentUnitExpenditureNote

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20190107082152_Initial_Garment_Unit_Expenditure_Note.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20190107082152_Initial_Garment_Unit_Expenditure_Note.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190107082152_Initial_Garment_Unit_Expenditure_Note")]
+    partial class Initial_Garment_Unit_Expenditure_Note
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
