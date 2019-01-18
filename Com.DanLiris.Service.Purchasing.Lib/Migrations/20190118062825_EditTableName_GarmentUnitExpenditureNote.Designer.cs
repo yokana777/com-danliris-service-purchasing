78de70c59389c66c07be385709b6706d7cf56d5a
Wahyu Widiarto <43228909+widiartowahyu3@users.noreply.github.com>
2019-01-18 13:30:01 +0700

    update table name Garment Unit Expenditure Note

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20190118062825_EditTableName_GarmentUnitExpenditureNote.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20190118062825_EditTableName_GarmentUnitExpenditureNote.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190118062825_EditTableName_GarmentUnitExpenditureNote")]
+    partial class EditTableName_GarmentUnitExpenditureNote
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
