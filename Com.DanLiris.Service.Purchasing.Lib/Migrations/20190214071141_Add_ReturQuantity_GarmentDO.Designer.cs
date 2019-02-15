a4426e290366809d174cd59a433e3ba0fc0aafb6
achensetyawati <achenzetya@gmail.com>
2019-02-15 09:18:51 +0700

    add returQuantity to Garment DO

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20190214071141_Add_ReturQuantity_GarmentDO.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20190214071141_Add_ReturQuantity_GarmentDO.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190214071141_Add_ReturQuantity_GarmentDO")]
+    partial class Add_ReturQuantity_GarmentDO
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
