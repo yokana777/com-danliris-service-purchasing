da27370ad4bd3d5c5ecc3cdfb6c338889bc62892
achensetyawati <achenzetya@gmail.com>
2019-03-16 08:55:39 +0700

    add incometaxby to EPO

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20190315070941_add_IncomeTaxBy_EPO.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20190315070941_add_IncomeTaxBy_EPO.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190315070941_add_IncomeTaxBy_EPO")]
+    partial class add_IncomeTaxBy_EPO
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
