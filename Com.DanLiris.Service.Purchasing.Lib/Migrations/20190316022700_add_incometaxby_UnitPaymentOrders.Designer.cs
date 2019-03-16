8548cbf045348bea03bd481a49c6a7be0fb44fda
achensetyawati <achenzetya@gmail.com>
2019-03-16 11:55:42 +0700

    add incometaxby UPO

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20190316022700_add_incometaxby_UnitPaymentOrders.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20190316022700_add_incometaxby_UnitPaymentOrders.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190316022700_add_incometaxby_UnitPaymentOrders")]
+    partial class add_incometaxby_UnitPaymentOrders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
