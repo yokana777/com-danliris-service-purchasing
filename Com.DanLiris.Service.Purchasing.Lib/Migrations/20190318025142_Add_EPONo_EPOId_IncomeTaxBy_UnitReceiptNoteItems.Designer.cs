4a609ad2d6e38fdb507ee2ab731889bf5a8374dd
achensetyawati <achenzetya@gmail.com>
2019-03-18 09:55:42 +0700

    Unit receipt note migration

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20190318025142_Add_EPONo_EPOId_IncomeTaxBy_UnitReceiptNoteItems.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20190318025142_Add_EPONo_EPOId_IncomeTaxBy_UnitReceiptNoteItems.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190318025142_Add_EPONo_EPOId_IncomeTaxBy_UnitReceiptNoteItems")]
+    partial class Add_EPONo_EPOId_IncomeTaxBy_UnitReceiptNoteItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
