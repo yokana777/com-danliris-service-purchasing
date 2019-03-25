b1a2a5d9053ebf9922eba6576e621f809be4328a
LeslieAula <LeslieAula@DESKTOP-FKFTBC0>
2019-03-25 23:26:03 +0700

    Add Categories

--- Com.DanLiris.Service.Purchasing.Lib/Facades/UnitReceiptNoteFacade/UnitReceiptNoteFacade.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Facades/UnitReceiptNoteFacade/UnitReceiptNoteFacade.cs
@@ -252,8 +252,8 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.UnitReceiptNoteFacade
                 var incomeTaxCOACode = "";
                 if (purchaseRequest != null)
                 {
-                    purchasingCOACode = COAGenerator.GetPurchasingCOA(purchaseRequest.DivisionName, purchaseRequest.UnitCode, purchaseRequest.CategoryName);
-                    stockCOACode = COAGenerator.GetStockCOA(purchaseRequest.DivisionName, purchaseRequest.UnitCode, purchaseRequest.CategoryName);
+                    purchasingCOACode = COAGenerator.GetPurchasingCOA(purchaseRequest.DivisionName, purchaseRequest.UnitCode, purchaseRequest.CategoryCode);
+                    stockCOACode = COAGenerator.GetStockCOA(purchaseRequest.DivisionName, purchaseRequest.UnitCode, purchaseRequest.CategoryCode);
                     debtCOACode = COAGenerator.GetDebtCOA(model.SupplierIsImport, purchaseRequest.DivisionName, purchaseRequest.UnitCode);
                     if (poExternal.UseIncomeTax && double.Parse(poExternal.IncomeTaxRate) > 0)
                         incomeTaxCOACode = COAGenerator.GetIncomeTaxCOA(poExternal.IncomeTaxName, purchaseRequest.DivisionName, purchaseRequest.UnitCode);
