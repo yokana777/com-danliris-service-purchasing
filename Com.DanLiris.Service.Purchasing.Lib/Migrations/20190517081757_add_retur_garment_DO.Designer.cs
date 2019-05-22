151ff5e1cdca594ccd989a7dc94a5761bc6263ee
achensetyawati <achenzetya@gmail.com>
2019-05-22 11:45:45 +0700

    table migration

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20190517081757_add_retur_garment_DO.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20190517081757_add_retur_garment_DO.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190517081757_add_retur_garment_DO")]
+    partial class add_retur_garment_DO
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -3280,7 +3281,7 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 
                     b.Property<string>("RONo");
 
-                    b.Property<double>("ReturQuantity");
+                    b.Property<double?>("ReturQuantity");
 
                     b.Property<long?>("ReturUomId");
 
--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20190521090518_update_unit_DO_item.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20190521090518_update_unit_DO_item.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190521090518_update_unit_DO_item")]
+    partial class update_unit_DO_item
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
