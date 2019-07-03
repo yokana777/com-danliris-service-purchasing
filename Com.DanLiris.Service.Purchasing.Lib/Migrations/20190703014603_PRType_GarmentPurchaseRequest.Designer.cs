978890e271bf2ca80fa34776976164167e64683b
kuswandanu <kuswandanu@gmail.com>
2019-07-03 09:38:16 +0700

    Add PRType and some columns to GarmentPurchaseRequest

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20190703014603_PRType_GarmentPurchaseRequest.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20190703014603_PRType_GarmentPurchaseRequest.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190703014603_PRType_GarmentPurchaseRequest")]
+    partial class PRType_GarmentPurchaseRequest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
