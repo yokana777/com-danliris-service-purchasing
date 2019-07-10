08e354e00d96cfe49e2ac2a6633e3903e05f92fa
achensetyawati <achenzetya@gmail.com>
2019-07-10 11:45:07 +0700

    Add columns in Garment Unit Receipt Notes

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20190709033658_Update_GarmentUnitReceiptNotes_add_columns.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20190709033658_Update_GarmentUnitReceiptNotes_add_columns.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190709033658_Update_GarmentUnitReceiptNotes_add_columns")]
+    partial class Update_GarmentUnitReceiptNotes_add_columns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
