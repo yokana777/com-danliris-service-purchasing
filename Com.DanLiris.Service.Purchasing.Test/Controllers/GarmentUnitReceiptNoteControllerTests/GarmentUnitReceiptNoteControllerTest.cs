8999c79942a0101de6042a86ddb58c7eef579062
cloudbagus <cloudbagus@gmail.com>
2019-07-24 12:16:46 +0700

    Laporan Flow Penerimaan
    menambahkan service laporan  flow detail penerimaan

--- Com.DanLiris.Service.Purchasing.Test/Controllers/GarmentUnitReceiptNoteControllerTests/GarmentUnitReceiptNoteControllerTest.cs
+++ Com.DanLiris.Service.Purchasing.Test/Controllers/GarmentUnitReceiptNoteControllerTests/GarmentUnitReceiptNoteControllerTest.cs
@@ -455,5 +455,50 @@ namespace Com.DanLiris.Service.Purchasing.Test.Controllers.GarmentUnitReceiptNot
             var response = controller.GetByUser();
             Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
         }
+
+        #region Laporan FLow Penerimaan 
+
+        [Fact]
+        public void Should_Success_Get_Report_Flow()
+        {
+            var mockFacade = new Mock<IGarmentUnitReceiptNoteFacade>();
+
+            mockFacade.Setup(x => x.GetReportFlow(It.IsAny<DateTime>(), It.IsAny<DateTime>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<int>()))
+                .Returns(Tuple.Create(new List<FlowDetailPenerimaanViewModels>(), 5));
+
+            var mockMapper = new Mock<IMapper>();
+
+            GarmentUnitReceiptNoteController controller = GetController(mockFacade, null, mockMapper);
+            var response = controller.GetReportDO(It.IsAny<DateTime>(), It.IsAny<DateTime>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>());
+            Assert.Equal((int)HttpStatusCode.OK, GetStatusCode(response));
+        }
+
+        [Fact]
+        public void Should_Error_Get_Report_All()
+        {
+            var mockFacade = new Mock<IGarmentUnitReceiptNoteFacade>();
+
+            var mockMapper = new Mock<IMapper>();
+
+
+            GarmentUnitReceiptNoteController controller = GetController(mockFacade, null, mockMapper);
+            var response = controller.GetReportDO(It.IsAny<DateTime>(), It.IsAny<DateTime>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>());
+            Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
+        }
+
+        [Fact]
+        public void Should_Error_Get_Xls_All()
+        {
+            var mockFacade = new Mock<IGarmentUnitReceiptNoteFacade>();
+
+            var mockMapper = new Mock<IMapper>();
+
+            GarmentUnitReceiptNoteController controller = new GarmentUnitReceiptNoteController(GetServiceProvider().Object, mockMapper.Object, mockFacade.Object);
+
+            var response = controller.GetXls(It.IsAny<DateTime>(), It.IsAny<DateTime>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<string>());
+            Assert.Equal((int)HttpStatusCode.InternalServerError, GetStatusCode(response));
+
+        }
+        #endregion
     }
 }
--- Com.DanLiris.Service.Purchasing.Test/Facades/GarmentUnitReceiptNoteFacadeTests/GarmentUnitReceiptNoteFacadeTest.cs
+++ Com.DanLiris.Service.Purchasing.Test/Facades/GarmentUnitReceiptNoteFacadeTests/GarmentUnitReceiptNoteFacadeTest.cs
@@ -1068,5 +1068,49 @@ namespace Com.DanLiris.Service.Purchasing.Test.Facades.GarmentUnitReceiptNoteFac
 
             Assert.IsType(typeof(System.IO.MemoryStream), Response);
         }
+
+
+
+        #region flow detail penerimaan 
+
+        [Fact]
+        public async void Should_Success_Get_FlowReport_Data()
+        {
+            GarmentUnitReceiptNoteFacade facade = new GarmentUnitReceiptNoteFacade(GetServiceProvider(), _dbContext(GetCurrentMethod()));
+            var model = await dataUtil(facade, GetCurrentMethod()).GetTestData();
+            var Response = facade.GetReportFlow(DateTime.MinValue, DateTime.MaxValue, model.UnitCode, "BAHAN BAKU", 1, 25, "{}", 7);
+            Assert.NotEqual(Response.Item1.Count, 0);
+        }
+
+        [Fact]
+        public async void Should_Success_Get_FlowReport_Data_Null_Parameter()
+        {
+            GarmentUnitReceiptNoteFacade facade = new GarmentUnitReceiptNoteFacade(GetServiceProvider(), _dbContext(GetCurrentMethod()));
+            var model = await dataUtil(facade, GetCurrentMethod()).GetTestData();
+            var Response = facade.GetReportFlow(null, null, "", "", 1, 25, "{}", 7);
+            Assert.NotEqual(Response.Item1.Count, 0);
+        }
+
+        [Fact]
+        public async void Should_Success_Get_FlowReport_Data_Excel()
+        {
+            GarmentUnitReceiptNoteFacade facade = new GarmentUnitReceiptNoteFacade(GetServiceProvider(), _dbContext(GetCurrentMethod()));
+            var model = await dataUtil(facade, GetCurrentMethod()).GetTestData();
+            var Response = facade.GenerateExcelLow(DateTime.MinValue, DateTime.MaxValue, model.UnitCode, "BAHAN BAKU", 7);
+            Assert.IsType(typeof(System.IO.MemoryStream), Response);
+        }
+
+        [Fact]
+        public async void Should_Success_Get_Report_Data_Excel_Null_parameter()
+        {
+            GarmentUnitReceiptNoteFacade facade = new GarmentUnitReceiptNoteFacade(GetServiceProvider(), _dbContext(GetCurrentMethod()));
+            var model = await dataUtil(facade, GetCurrentMethod()).GetTestData();
+            var Response = facade.GenerateExcelLow(null, null, "", "", 7);
+            Assert.IsType(typeof(System.IO.MemoryStream), Response);
+        }
+
+        #endregion
     }
+
+
 }
