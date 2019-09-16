4c0f56986b46300156ee11bff53525fb0f722b55
kuswandanu <kuswandanu@gmail.com>
2019-09-16 10:46:14 +0700

    Add_MaxLength_Some_Columns Migration

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20190916034540_Add_MaxLength_Some_Columns.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20190916034540_Add_MaxLength_Some_Columns.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190916034540_Add_MaxLength_Some_Columns")]
+    partial class Add_MaxLength_Some_Columns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
