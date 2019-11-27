30d4efeb6f0a989d1e556ec30a11582018dec454
achensetyawati <achenzetya@gmail.com>
2019-11-27 10:03:45 +0700

    Update Garment Unit DO table

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20191127022436_update_GarmentUnitDeliveryOrders.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20191127022436_update_GarmentUnitDeliveryOrders.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191127022436_update_GarmentUnitDeliveryOrders")]
+    partial class update_GarmentUnitDeliveryOrders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
