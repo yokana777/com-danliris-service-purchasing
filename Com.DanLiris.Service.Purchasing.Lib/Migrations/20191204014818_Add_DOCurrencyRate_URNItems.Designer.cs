fa343fb35912741473ea672f57c8989bc2e07b8d
achensetyawati <achenzetya@gmail.com>
2019-12-04 10:04:09 +0700

    Add DOCurrencyRate to GarmentURNItems

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20191204014818_Add_DOCurrencyRate_URNItems.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20191204014818_Add_DOCurrencyRate_URNItems.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191204014818_Add_DOCurrencyRate_URNItems")]
+    partial class Add_DOCurrencyRate_URNItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
