89c338daa5ffdb1bef5584d04792a3e3e37da1a5
Leslie Aula <leslie.aula@moonlay.com>
2019-12-23 11:16:06 +0700

    Add Test Coverage

---
+++ Com.DanLiris.Service.Purchasing.Test/Utils/CoreDataTest.cs
@@ -0,0 +1,18 @@
+﻿using Com.DanLiris.Service.Purchasing.Lib.ViewModels.ExternalPurchaseOrderViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.DanLiris.Service.Purchasing.Test.Utils
+{
+    public class CoreDataTest
+    {
+        [Fact]
+        public void Should_Success_Build_ExternalPurchaseDeliveryOrderDurationReportViewModel()
+        {
+            var viewModel = new ExternalPurchaseDeliveryOrderDurationReportViewModel();
+            Assert.NotNull(viewModel);
+        }
+    }
+}
