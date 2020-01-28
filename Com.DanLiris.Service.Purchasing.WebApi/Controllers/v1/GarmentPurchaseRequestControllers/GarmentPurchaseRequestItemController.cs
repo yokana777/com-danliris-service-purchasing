e3054d956d0a8207727d3809e80075a9e999c738
kuswandanu <kuswandanu@gmail.com>
2020-01-28 09:13:06 +0700

    GarmentPurchaseRequestItem Patch

---
+++ Com.DanLiris.Service.Purchasing.WebApi/Controllers/v1/GarmentPurchaseRequestControllers/GarmentPurchaseRequestItemController.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using Com.DanLiris.Service.Purchasing.Lib.Interfaces;
+using Com.DanLiris.Service.Purchasing.Lib.Models.GarmentPurchaseRequestModel;
+using Com.DanLiris.Service.Purchasing.Lib.Services;
+using Com.DanLiris.Service.Purchasing.WebApi.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Com.DanLiris.Service.Purchasing.WebApi.Controllers.v1.GarmentPurchaseRequestControllers
+{
+    [Produces("application/json")]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/garment-purchase-requests/items")]
+    [Authorize]
+    public class GarmentPurchaseRequestItemController : Controller
+    {
+        private string ApiVersion = "1.0.0";
+        public readonly IServiceProvider serviceProvider;
+        private readonly IMapper mapper;
+        private readonly IGarmentPurchaseRequestItemFacade facade;
+        private readonly IdentityService identityService;
+
+        public GarmentPurchaseRequestItemController(IServiceProvider serviceProvider, IMapper mapper, IGarmentPurchaseRequestItemFacade facade)
+        {
+            this.serviceProvider = serviceProvider;
+            this.mapper = mapper;
+            this.facade = facade;
+            identityService = (IdentityService)serviceProvider.GetService(typeof(IdentityService));
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> PatchOne([FromBody]JsonPatchDocument<GarmentPurchaseRequestItem> jsonPatch, int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                identityService.Username = User.Claims.Single(p => p.Type.Equals("username")).Value;
+
+                await facade.Patch($"[{id}]", jsonPatch);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.INTERNAL_ERROR_STATUS_CODE, e.Message)
+                    .Fail();
+                return StatusCode(General.INTERNAL_ERROR_STATUS_CODE, Result);
+            }
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> Patch([FromBody]JsonPatchDocument<GarmentPurchaseRequestItem> jsonPatch, string id = "[]")
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                identityService.Username = User.Claims.Single(p => p.Type.Equals("username")).Value;
+
+                await facade.Patch(id, jsonPatch);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.INTERNAL_ERROR_STATUS_CODE, e.Message)
+                    .Fail();
+                return StatusCode(General.INTERNAL_ERROR_STATUS_CODE, Result);
+            }
+        }
+    }
+}
