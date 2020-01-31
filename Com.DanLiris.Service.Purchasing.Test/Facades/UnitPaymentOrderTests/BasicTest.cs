7755ff1e4a55c537ee1242057d5352349fb1df69
Leslie Aula <leslie.aula@moonlay.com>
2020-01-31 18:13:01 +0700

    Add Test Coverage

--- Com.DanLiris.Service.Purchasing.Test/Facades/UnitPaymentOrderTests/BasicTest.cs
+++ Com.DanLiris.Service.Purchasing.Test/Facades/UnitPaymentOrderTests/BasicTest.cs
@@ -1,4 +1,5 @@
 ﻿using Com.DanLiris.Service.Purchasing.Lib;
+using Com.DanLiris.Service.Purchasing.Lib.Enums;
 using Com.DanLiris.Service.Purchasing.Lib.Facades;
 using Com.DanLiris.Service.Purchasing.Lib.Facades.ExternalPurchaseOrderFacade;
 using Com.DanLiris.Service.Purchasing.Lib.Facades.InternalPO;
@@ -6,6 +7,7 @@ using Com.DanLiris.Service.Purchasing.Lib.Facades.UnitReceiptNoteFacade;
 using Com.DanLiris.Service.Purchasing.Lib.Interfaces;
 using Com.DanLiris.Service.Purchasing.Lib.Services;
 using Com.DanLiris.Service.Purchasing.Lib.Utilities.CacheManager;
+using Com.DanLiris.Service.Purchasing.Lib.Utilities.CacheManager.CacheData;
 using Com.DanLiris.Service.Purchasing.Lib.Utilities.Currencies;
 using Com.DanLiris.Service.Purchasing.Lib.ViewModels.UnitPaymentOrderViewModel;
 using Com.DanLiris.Service.Purchasing.Test.DataUtils.DeliveryOrderDataUtils;
@@ -302,6 +304,9 @@ namespace Com.DanLiris.Service.Purchasing.Test.Facades.UnitPaymentOrderTests
         [Fact]
         public async Task Should_Success_Create_DataWithVAT()
         {
+            var serviceProvider = GetServiceProvider(GetCurrentMethod()).Object;
+            var memoryCacheService = serviceProvider.GetService<IMemoryCacheManager>();
+            memoryCacheService.Set(MemoryCacheConstant.Categories, new List<CategoryCOAResult>() { new CategoryCOAResult() { _id = 1 } });
 
             UnitPaymentOrderFacade facade = new UnitPaymentOrderFacade(GetServiceProvider(GetCurrentMethod()).Object, _dbContext(GetCurrentMethod()));
 
