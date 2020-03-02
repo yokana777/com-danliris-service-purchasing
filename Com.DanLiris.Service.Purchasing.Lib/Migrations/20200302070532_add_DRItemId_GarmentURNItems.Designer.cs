d7f87675bac32d53adfc6c66513a0701280c12de
achensetyawati <achenzetya@gmail.com>
2020-03-02 14:26:55 +0700

    Update Garment URN Models

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20200302070532_add_DRItemId_GarmentURNItems.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20200302070532_add_DRItemId_GarmentURNItems.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200302070532_add_DRItemId_GarmentURNItems")]
+    partial class add_DRItemId_GarmentURNItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
