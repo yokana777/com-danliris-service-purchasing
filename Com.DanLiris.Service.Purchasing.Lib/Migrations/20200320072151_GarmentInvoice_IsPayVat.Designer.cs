bb4609daf47402ccef163550e382386bfc6093d2
kuswandanu <kuswandanu@gmail.com>
2020-03-20 14:23:59 +0700

    GarmentInvoice IsPayVat

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20200320072151_GarmentInvoice_IsPayVat.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20200320072151_GarmentInvoice_IsPayVat.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200320072151_GarmentInvoice_IsPayVat")]
+    partial class GarmentInvoice_IsPayVat
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
