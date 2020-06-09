352627fe6a9f3774d2f3d14f9cfd33deff7f52ce
achensetyawati <achenzetya@gmail.com>
2020-06-09 10:17:20 +0700

    UEN Garment DO_CurrencyRate 0 unit test

--- Com.DanLiris.Service.Purchasing.Test/DataUtils/GarmentUnitDeliveryOrderDataUtils/GarmentUnitDeliveryOrderDataUtil.cs
+++ Com.DanLiris.Service.Purchasing.Test/DataUtils/GarmentUnitDeliveryOrderDataUtils/GarmentUnitDeliveryOrderDataUtil.cs
@@ -178,9 +178,105 @@ namespace Com.DanLiris.Service.Purchasing.Test.DataUtils.GarmentUnitDeliveryOrde
             return garmentUnitDeliveryOrder;
         }
 
+        public async Task<GarmentUnitDeliveryOrder> GetNewDataMultipleItem_DOCurrency(GarmentUnitReceiptNote unitReceiptNote1 = null, GarmentUnitReceiptNote unitReceiptNote2 = null)
+        {
+            DateTimeOffset now = DateTimeOffset.Now;
+            long nowTicks = now.Ticks;
+
+            var garmentUnitReceiptNote1 = unitReceiptNote1 ?? await Task.Run(() => UNDataUtil.GetNewData3());
+            var garmentUnitReceiptNote2 = unitReceiptNote2 ?? await Task.Run(() => UNDataUtil.GetNewData3(nowTicks + 1));
+            GarmentUnitDeliveryOrder garmentUnitDeliveryOrder = new GarmentUnitDeliveryOrder
+            {
+                UnitDOType = "SAMPLE",
+                DOId = garmentUnitReceiptNote1.DOId,
+                UnitDODate = DateTimeOffset.Now,
+                UnitSenderId = garmentUnitReceiptNote1.UnitId,
+                UnitRequestCode = garmentUnitReceiptNote1.UnitCode,
+                UnitRequestName = garmentUnitReceiptNote1.UnitName,
+                UnitRequestId = garmentUnitReceiptNote1.UnitId,
+                UnitSenderCode = garmentUnitReceiptNote1.UnitCode,
+                UnitSenderName = garmentUnitReceiptNote1.UnitName,
+                StorageId = garmentUnitReceiptNote1.StorageId,
+                StorageCode = garmentUnitReceiptNote1.StorageCode,
+                StorageName = garmentUnitReceiptNote1.StorageName,
+                RONo = garmentUnitReceiptNote1.Items.Select(i => i.RONo).FirstOrDefault(),
+                Article = $"Article{nowTicks}",
+                Items = new List<GarmentUnitDeliveryOrderItem>()
+            };
+
+            foreach (var item in garmentUnitReceiptNote1.Items)
+            {
+                garmentUnitDeliveryOrder.Items.Add(
+                    new GarmentUnitDeliveryOrderItem
+                    {
+                        IsSave = true,
+                        DODetailId = item.DODetailId,
+                        EPOItemId = item.EPOItemId,
+                        POItemId = item.POItemId,
+                        PRItemId = item.PRItemId,
+                        FabricType = "FABRIC",
+                        URNId = garmentUnitReceiptNote1.Id,
+                        URNItemId = item.Id,
+                        URNNo = garmentUnitReceiptNote1.URNNo,
+                        POSerialNumber = item.POSerialNumber,
+                        RONo = item.RONo,
+                        ProductId = item.ProductId,
+                        ProductCode = item.ProductCode,
+                        ProductName = item.ProductName,
+                        Quantity = (double)(item.SmallQuantity - item.OrderQuantity),
+                        UomId = item.UomId,
+                        UomUnit = item.UomUnit,
+                        ReturUomId = item.UomId,
+                        ReturUomUnit = item.UomUnit,
+                        DOCurrencyRate = garmentUnitReceiptNote1.DOCurrencyRate
+                    });
+            }
+
+
+
+
+            foreach (var item in garmentUnitReceiptNote2.Items)
+            {
+                garmentUnitDeliveryOrder.Items.Add(
+                    new GarmentUnitDeliveryOrderItem
+                    {
+                        IsSave = true,
+                        DODetailId = item.DODetailId,
+                        EPOItemId = item.EPOItemId,
+                        POItemId = item.POItemId,
+                        PRItemId = item.PRItemId,
+                        FabricType = "FABRIC",
+                        URNId = garmentUnitReceiptNote1.Id,
+                        URNItemId = item.Id,
+                        URNNo = garmentUnitReceiptNote1.URNNo,
+                        POSerialNumber = item.POSerialNumber,
+                        RONo = item.RONo,
+                        ProductId = item.ProductId + 1,
+                        ProductCode = item.ProductCode + $"{nowTicks}",
+                        ProductName = item.ProductName + $"{nowTicks}",
+                        Quantity = (double)(item.SmallQuantity - item.OrderQuantity),
+                        UomId = item.UomId,
+                        UomUnit = item.UomUnit,
+                        DOCurrencyRate = garmentUnitReceiptNote1.DOCurrencyRate,
+                        ReturUomId = item.UomId,
+                        ReturUomUnit = item.UomUnit,
+                    });
+            }
+
+
+            return garmentUnitDeliveryOrder;
+        }
+
         public async Task<GarmentUnitDeliveryOrder> GetTestDataMultipleItem()
         {
             var data = await GetNewDataMultipleItem();
+            await facade.Create(data);
+            return data;
+        }
+
+        public async Task<GarmentUnitDeliveryOrder> GetTestDataMultipleItem_DOCurrency()
+        {
+            var data = await GetNewDataMultipleItem_DOCurrency();
             await facade.Create(data);
             return data;
         }
