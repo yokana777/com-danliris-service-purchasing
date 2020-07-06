74f1d5867c9c0e7da0842b2312e33fefea0b2bb9
leslieaula <cristiano.ulo@gmail.com>
2020-07-06 15:53:33 +0700

    add test coverage status ipo

--- Com.DanLiris.Service.Purchasing.WebApi/Controllers/v1/GarmentPurchaseRequestControllers/GarmentMonitoringPurchaseController.cs
+++ Com.DanLiris.Service.Purchasing.WebApi/Controllers/v1/GarmentPurchaseRequestControllers/GarmentMonitoringPurchaseController.cs
@@ -127,7 +127,7 @@ namespace Com.DanLiris.Service.Purchasing.WebApi.Controllers.v1.GarmentPurchaseR
 				byte[] xlsInBytes;
 
 				int offset = Convert.ToInt32(Request.Headers["x-timezone-offset"]);
-				var xls = facade.GenerateExcelByUserPurchase(epono, unit, roNo, article, poSerialNumber, username, doNo, ipoStatus, supplier, status, dateFrom, dateTo, dateFromEx, dateToEx, page, size, Order, offset);
+				var xls = facade.GenerateExcelPurchase(epono, unit, roNo, article, poSerialNumber, username, doNo, ipoStatus, supplier, status, dateFrom, dateTo, dateFromEx, dateToEx, page, size, Order, offset);
 
 				string filename = "Laporan Pembelian Garment - " + username;
 				if (dateFrom != null) filename += " " + ((DateTime)dateFrom).ToString("dd-MM-yyyy");
