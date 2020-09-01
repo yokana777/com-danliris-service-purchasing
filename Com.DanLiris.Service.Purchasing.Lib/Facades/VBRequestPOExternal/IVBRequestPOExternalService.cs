3b39208492c481cd29dfc057c00c482745363703
leslieaula <cristiano.ulo@gmail.com>
2020-09-01 10:35:25 +0700

    Add Currency Code Filter

--- Com.DanLiris.Service.Purchasing.Lib/Facades/VBRequestPOExternal/IVBRequestPOExternalService.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Facades/VBRequestPOExternal/IVBRequestPOExternalService.cs
@@ -7,7 +7,7 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.VBRequestPOExternal
     public interface IVBRequestPOExternalService
     {
         List<POExternalDto> ReadPOExternal(string keyword, string division, string currencyCode);
-        List<SPBDto> ReadSPB(string keyword, string division, List<int> epoIds);
+        List<SPBDto> ReadSPB(string keyword, string division, List<int> epoIds, string currencyCode);
         int UpdateSPB(string division, int spbId);
     }
 }
