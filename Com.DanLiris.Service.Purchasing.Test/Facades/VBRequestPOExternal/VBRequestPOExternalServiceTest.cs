46a30863b4c6edcc24fa75407b0b97abf9c1d4a1
adepamungkas <ade.pamungkas9@gmail.com>
2020-09-02 13:37:08 +0700

    VBRequestPOExternalControllerTest

---
+++ Com.DanLiris.Service.Purchasing.Test/Facades/VBRequestPOExternal/VBRequestPOExternalServiceTest.cs
@@ -0,0 +1,77 @@
+﻿using Com.DanLiris.Service.Purchasing.Lib;
+using Com.DanLiris.Service.Purchasing.Lib.Facades.VBRequestPOExternal;
+using Com.DanLiris.Service.Purchasing.Lib.Interfaces;
+using Com.DanLiris.Service.Purchasing.Lib.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xunit;
+
+namespace Com.DanLiris.Service.Purchasing.Test.Facades.VBRequestPOExternal
+{
+   public class VBRequestPOExternalServiceTest
+    {
+
+        protected string GetCurrentAsyncMethod([CallerMemberName] string methodName = "")
+        {
+            var method = new StackTrace()
+                .GetFrames()
+                .Select(frame => frame.GetMethod())
+                .FirstOrDefault(item => item.Name == methodName);
+
+            return method.Name;
+
+        }
+
+        protected PurchasingDbContext GetDbContext(string testName)
+        {
+            string databaseName = testName;
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var optionsBuilder = new DbContextOptionsBuilder<PurchasingDbContext>();
+            optionsBuilder
+                .UseInMemoryDatabase(databaseName)
+                .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                 .UseInternalServiceProvider(serviceProvider);
+
+            PurchasingDbContext DbContex = new PurchasingDbContext(optionsBuilder.Options);
+            return DbContex;
+        }
+
+        private Mock<IServiceProvider> GetServiceProvider()
+        {
+           
+            var serviceProvider = new Mock<IServiceProvider>();
+            serviceProvider
+                .Setup(x => x.GetService(typeof(IdentityService)))
+                .Returns(new IdentityService() { Token = "Token", Username = "Test" });
+
+            //serviceProvider
+            //    .Setup(x => x.GetService(typeof(IHttpClientService)))
+            //    .Returns(HttpClientService.Object);
+
+            return serviceProvider;
+        }
+
+        [Fact]
+        public void ShouldSuccess_ReadPOExternal()
+        {
+            PurchasingDbContext dbContext = GetDbContext(GetCurrentAsyncMethod());
+            var serviceProviderMock = GetServiceProvider();
+
+            VBRequestPOExternalService service = new VBRequestPOExternalService(dbContext, serviceProviderMock.Object);
+            service.ReadPOExternal("", "", "IDR");
+
+        }
+
+    }
+}
