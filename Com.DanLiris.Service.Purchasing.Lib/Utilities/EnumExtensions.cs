dfa34047d0c6d5e1755fd9ca692f9102d9b48ed3
Leslie Aula <leslie.aula@moonlay.com>
2020-12-07 14:02:21 +0700

    Fix Bugs show enum display name

--- Com.DanLiris.Service.Purchasing.Lib/Utilities/EnumExtensions.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Utilities/EnumExtensions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Text;
+using System.Reflection;
 
 namespace Com.DanLiris.Service.Purchasing.Lib.Utilities
 {
