148659a083909c78c6cb16de66230204e9e97256
Leslie Aula <leslie.aula@moonlay.com>
2020-12-12 02:32:52 +0700

    Add Get Calculation

--- Com.DanLiris.Service.Purchasing.Lib/Facades/DebtAndDispositionSummary/UnitDto.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Facades/DebtAndDispositionSummary/UnitDto.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public int AccountingUnitId { get; set; }
         public int DivisionId { get; set; }
+        public string Name { get; set; }
     }
 }
