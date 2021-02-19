6ea7913fb785daee3cf1dd04ef03878152d6eaa6
ryan <ryan.santosa@moonlay.com>
2021-02-19 18:57:11 +0700

    fixing layout excel

--- Com.DanLiris.Service.Purchasing.Lib/Facades/GarmentPurchasingBookReport/Excel/GarmentPurchasingBookReportImportExcel.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Facades/GarmentPurchasingBookReport/Excel/GarmentPurchasingBookReportImportExcel.cs
@@ -16,14 +16,14 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentPurchasingBookRepor
             var reportDataTable = new DataTable();
             reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Tanggal Bon", DataType = typeof(string) });
             reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Supplier", DataType = typeof(string) });
-            reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Keterangan", DataType = typeof(string) });
+            reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Nama Barang", DataType = typeof(string) });
             reportDataTable.Columns.Add(new DataColumn() { ColumnName = "No Surat Jalan", DataType = typeof(string) });
             reportDataTable.Columns.Add(new DataColumn() { ColumnName = "No BP Besar", DataType = typeof(string) });
             reportDataTable.Columns.Add(new DataColumn() { ColumnName = "No BP Kecil", DataType = typeof(string) });
             reportDataTable.Columns.Add(new DataColumn() { ColumnName = "No Invoice", DataType = typeof(string) });
             reportDataTable.Columns.Add(new DataColumn() { ColumnName = "No Faktur Pajak", DataType = typeof(string) });
             reportDataTable.Columns.Add(new DataColumn() { ColumnName = "No NI", DataType = typeof(string) });
-            reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Kategori Pembelian", DataType = typeof(string) });
+            //reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Kategori Pembelian", DataType = typeof(string) });
             reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Kategori Pembukuan", DataType = typeof(string) });
 
             reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Tanggal BC", DataType = typeof(string) });
@@ -33,19 +33,20 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentPurchasingBookRepor
 
 
             reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Mata Uang", DataType = typeof(string) });
-            reportDataTable.Columns.Add(new DataColumn() { ColumnName = "DPP Valas", DataType = typeof(decimal) });
-            reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Rate", DataType = typeof(decimal) });
+            reportDataTable.Columns.Add(new DataColumn() { ColumnName = "DPP Valas", DataType = typeof(string) });
+            reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Rate", DataType = typeof(string) });
 
-            reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Total(IDR)", DataType = typeof(decimal) });
+            reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Total(IDR)", DataType = typeof(string) });
 
             var categoryDataTable = new DataTable();
             categoryDataTable.Columns.Add(new DataColumn() { ColumnName = "Kategori", DataType = typeof(string) });
-            categoryDataTable.Columns.Add(new DataColumn() { ColumnName = "Total", DataType = typeof(decimal) });
+            categoryDataTable.Columns.Add(new DataColumn() { ColumnName = "Total", DataType = typeof(string) });
+            categoryDataTable.Columns.Add(new DataColumn() { ColumnName = "Total (Valas)", DataType = typeof(string) });
 
             var currencyDataTable = new DataTable();
             currencyDataTable.Columns.Add(new DataColumn() { ColumnName = "Mata Uang", DataType = typeof(string) });
-            currencyDataTable.Columns.Add(new DataColumn() { ColumnName = "Total", DataType = typeof(decimal) });
-            currencyDataTable.Columns.Add(new DataColumn() { ColumnName = "Total (IDR)", DataType = typeof(decimal) });
+            currencyDataTable.Columns.Add(new DataColumn() { ColumnName = "Total", DataType = typeof(string) });
+            currencyDataTable.Columns.Add(new DataColumn() { ColumnName = "Total (IDR)", DataType = typeof(string) });
 
             if (result.Data.Count > 0)
             {
@@ -61,16 +62,24 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentPurchasingBookRepor
                         report.InvoiceNo,
                         report.VATNo,
                         report.InternalNoteNo,
-                        report.PurchasingCategoryName,
+                        //report.PurchasingCategoryName,
                         report.AccountingCategoryName,
 
-                        report.CustomsArrivalDate, report.CurrencyCode, report.DPPAmount, report.VATAmount, report.IncomeTaxAmount, report.Total);
+                        report.CustomsDate.AddHours(timeZone).ToString("dd/MM/yyyy"),
+                        report.CustomsNo,
+                        report.CustomsType,
+                        report.ImportValueRemark,
+                        report.CurrencyCode, 
+                        report.DPPAmount.ToString("C2"), 
+                        //report.VATAmount.ToString("C2"), 
+                        report.IncomeTaxAmount.ToString("C2"),
+                        report.Total.ToString("C2"));
                 }
                 foreach (var categorySummary in result.Categories)
-                    categoryDataTable.Rows.Add(categorySummary.CategoryName, categorySummary.Amount);
+                    categoryDataTable.Rows.Add(categorySummary.CategoryName, categorySummary.Amount.ToString("C2"),categorySummary.Amount.ToString("C2"));
 
                 foreach (var currencySummary in result.Currencies)
-                    currencyDataTable.Rows.Add(currencySummary.CurrencyCode, currencySummary.Amount, currencySummary.Amount);//TODO : change to Currency TOtal Idr
+                    currencyDataTable.Rows.Add(currencySummary.CurrencyCode, currencySummary.Amount.ToString("C2"), currencySummary.Amount.ToString("C2"));//TODO : change to Currency TOtal Idr
             }
 
             using (var package = new ExcelPackage())
@@ -97,39 +106,68 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentPurchasingBookRepor
                         var rowStartHeaderSpan = rowStartHeader + 1;
                         worksheet.Cells[rowStartHeaderSpan, colStartHeader].Value = column.ColumnName;
                         worksheet.Cells[rowStartHeader, colStartHeader].Value = "Bea Cukai";
+                        worksheet.Cells[rowStartHeaderSpan, colStartHeader].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
+                        worksheet.Cells[rowStartHeaderSpan, colStartHeader].Style.Font.Bold = true;
+                        worksheet.Cells[rowStartHeaderSpan, colStartHeader].Style.HorizontalAlignment= OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                        worksheet.Cells[rowStartHeaderSpan, colStartHeader].Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
+
+
                         worksheet.Cells[rowStartHeader, colStartHeader, rowStartHeader, colStartHeader + 3].Merge = true;
                         worksheet.Cells[rowStartHeader, colStartHeader, rowStartHeader, colStartHeader + 3].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
-                    }
-                    else if (column.ColumnName == "No BC" || column.ColumnName == "Jenis BC" || column.ColumnName == "Ket Nilai Impor")
-                    {
-                        var rowStartHeaderSpan = rowStartHeader + 1;
-                        worksheet.Cells[rowStartHeaderSpan, colStartHeader].Value = column.ColumnName;
+                        worksheet.Cells[rowStartHeader, colStartHeader, rowStartHeader, colStartHeader + 3].Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
+                        worksheet.Cells[rowStartHeader, colStartHeader, rowStartHeader, colStartHeader + 3].Style.Font.Bold = true;
+                        worksheet.Cells[rowStartHeader, colStartHeader, rowStartHeader, colStartHeader + 3].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
                     }
                     else if (column.ColumnName == "Mata Uang")
                     {
                         var rowStartHeaderSpan = rowStartHeader + 1;
                         worksheet.Cells[rowStartHeaderSpan, colStartHeader].Value = column.ColumnName;
                         worksheet.Cells[rowStartHeader, colStartHeader].Value = "Pembelian";
+                        worksheet.Cells[rowStartHeaderSpan, colStartHeader].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
+                        worksheet.Cells[rowStartHeaderSpan, colStartHeader].Style.Font.Bold = true;
+                        worksheet.Cells[rowStartHeaderSpan, colStartHeader].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                        worksheet.Cells[rowStartHeaderSpan, colStartHeader].Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
+
                         worksheet.Cells[rowStartHeader, colStartHeader, rowStartHeader, colStartHeader + 2].Merge = true;
                         worksheet.Cells[rowStartHeader, colStartHeader, rowStartHeader, colStartHeader + 2].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                        worksheet.Cells[rowStartHeader, colStartHeader, rowStartHeader, colStartHeader + 2].Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
+                        worksheet.Cells[rowStartHeader, colStartHeader, rowStartHeader, colStartHeader + 2].Style.Font.Bold = true;
+                        worksheet.Cells[rowStartHeader, colStartHeader, rowStartHeader, colStartHeader + 2].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
                     }
-                    else if (column.ColumnName == "DPP Valas" || column.ColumnName == "Rate")
+                    else if (column.ColumnName == "DPP Valas" || column.ColumnName == "Rate" || column.ColumnName == "No BC" || column.ColumnName == "Jenis BC" || column.ColumnName == "Ket Nilai Impor")
                     {
                         var rowStartHeaderSpan = rowStartHeader + 1;
                         worksheet.Cells[rowStartHeaderSpan, colStartHeader].Value = column.ColumnName;
+                        worksheet.Cells[rowStartHeaderSpan, colStartHeader].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
+                        worksheet.Cells[rowStartHeaderSpan, colStartHeader].Style.Font.Bold = true;
+                        worksheet.Cells[rowStartHeaderSpan, colStartHeader].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                        worksheet.Cells[rowStartHeaderSpan, colStartHeader].Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
                     }
                     else
                     {
                         worksheet.Cells[rowStartHeader, colStartHeader].Value = column.ColumnName;
+                        worksheet.Cells[rowStartHeader, colStartHeader].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
+                        worksheet.Cells[rowStartHeader, colStartHeader].Style.Font.Bold = true;
+
                         worksheet.Cells[rowStartHeader, colStartHeader, rowStartHeader + 1, colStartHeader].Merge = true;
                         worksheet.Cells[rowStartHeader, colStartHeader, rowStartHeader + 1, colStartHeader].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                        worksheet.Cells[rowStartHeader, colStartHeader, rowStartHeader + 1, colStartHeader].Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
+                        worksheet.Cells[rowStartHeader, colStartHeader, rowStartHeader + 1, colStartHeader].Style.Font.Bold =true;
+                        worksheet.Cells[rowStartHeader, colStartHeader, rowStartHeader + 1, colStartHeader].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
                     }
                     colStartHeader++;
                 }
                 #endregion
-                worksheet.Cells["A6"].LoadFromDataTable(reportDataTable, true);
-                worksheet.Cells[$"A{6 + 3 + result.Data.Count}"].LoadFromDataTable(categoryDataTable, true);
-                worksheet.Cells[$"A{6 + result.Data.Count + 3 + result.Data.Count + 3}"].LoadFromDataTable(currencyDataTable, true);
+                worksheet.Cells["A6"].LoadFromDataTable(reportDataTable, false);
+                for (int i = 6; i < result.Data.Count + 6; i++)
+                {
+                    for (int j = 1; j <= reportDataTable.Columns.Count; j++)
+                    {
+                        worksheet.Cells[i, j].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
+                    }
+                }
+                worksheet.Cells[$"A{6 + 3 + result.Data.Count}"].LoadFromDataTable(categoryDataTable, true,OfficeOpenXml.Table.TableStyles.Light18);
+                worksheet.Cells[$"A{6 + result.Data.Count + 3 + result.Data.Count + 3}"].LoadFromDataTable(currencyDataTable, true,OfficeOpenXml.Table.TableStyles.Light18);
 
                 var stream = new MemoryStream();
                 package.SaveAs(stream);
