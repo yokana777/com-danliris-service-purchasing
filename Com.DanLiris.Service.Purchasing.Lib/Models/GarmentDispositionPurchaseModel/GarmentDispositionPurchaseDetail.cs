80ff44119e8a4d6361ef92d648197b9a2853545c
ryan <ryan.santosa@moonlay.com>
2021-03-10 15:08:01 +0700

    addmigration

--- Com.DanLiris.Service.Purchasing.Lib/Models/GarmentDispositionPurchaseModel/GarmentDispositionPurchaseDetail.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Models/GarmentDispositionPurchaseModel/GarmentDispositionPurchaseDetail.cs
@@ -16,6 +16,7 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Models.GarmentDispositionPurchaseM
         public string ProductName { get; set; }
         public int UnitId { get; set; }
         public string UnitName { get; set; }
+        public string UnitCode { get; set; }
         public double QTYOrder { get; set; }
         public string QTYUnit { get; set; }
         public double QTYRemains { get; set; }
