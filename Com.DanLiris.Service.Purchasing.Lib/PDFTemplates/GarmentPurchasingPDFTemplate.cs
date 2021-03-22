7fe11a8fe93d7373881f35899b00bcf4b76c7e07
ryan <ryan.santosa@moonlay.com>
2021-03-22 11:27:57 +0700

    fixing calculatte beban unit in pdf disposition

--- Com.DanLiris.Service.Purchasing.Lib/PDFTemplates/GarmentPurchasingPDFTemplate.cs
+++ Com.DanLiris.Service.Purchasing.Lib/PDFTemplates/GarmentPurchasingPDFTemplate.cs
@@ -488,7 +488,11 @@ namespace Com.DanLiris.Service.Purchasing.Lib.PDFTemplates
                 ).ToList();
             foreach(var perUnit in AmountPerUnit)
             {
-                cellLeftNoBorder.Phrase = new Phrase($"- {perUnit.Key.UnitName} = {perUnit.Value.Sum(t=> (t.PaidPrice) + (t.PaidPrice*0.1) - (t.PaidPrice*(viewModel.Items.Where(a=> a.Id == t.GarmentDispositionPurchaseItemId).FirstOrDefault()?.IncomeTaxRate /100)))?.ToString("N", new CultureInfo("id-ID"))}", bold_font3); 
+                var sumPerUnit = perUnit.Value.Sum(t => 
+                (t.PaidPrice) + 
+                (viewModel.Items.Where(a => a.Id == t.GarmentDispositionPurchaseItemId).FirstOrDefault()?.IsUseVat? t.PaidPrice * 0.1:0) - 
+                (t.PaidPrice * (viewModel.Items.Where(a => a.Id == t.GarmentDispositionPurchaseItemId).FirstOrDefault()?.IncomeTaxRate / 100)))?.ToString("N", new CultureInfo("id-ID"));
+                cellLeftNoBorder.Phrase = new Phrase($"- {perUnit.Key.UnitName} = {sumPerUnit}", bold_font3); 
                 tableBeban.AddCell(cellLeftNoBorder);
             }
             PdfPCell cellBeban = new PdfPCell(tableBeban); // dont remove
