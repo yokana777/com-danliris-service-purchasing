bbf1edd25acadf2e52f700da11caf4098a89aed5
ryan <ryan.santosa@moonlay.com>
2021-03-22 15:09:40 +0700

    fixing calculate Total pembelian

--- Com.DanLiris.Service.Purchasing.Lib/PDFTemplates/GarmentPurchasingPDFTemplate.cs
+++ Com.DanLiris.Service.Purchasing.Lib/PDFTemplates/GarmentPurchasingPDFTemplate.cs
@@ -465,7 +465,9 @@ namespace Com.DanLiris.Service.Purchasing.Lib.PDFTemplates
             tableNote.AddCell(cellLeftNoBorder);
             cellLeftNoBorder.Phrase = new Phrase(":", normal_font);
             tableNote.AddCell(cellLeftNoBorder);
-            cellLeftNoBorder.Phrase = new Phrase($"{viewModel.CurrencyCode}" + " " + $"{(totalPurchase + ppnPurchase).ToString("N", new CultureInfo("id-ID"))}", normal_font);
+            //cellLeftNoBorder.Phrase = new Phrase($"{viewModel.CurrencyCode}" + " " + $"{(totalPurchase + ppnPurchase).ToString("N", new CultureInfo("id-ID"))}", normal_font);
+            cellLeftNoBorder.Phrase = new Phrase($"{viewModel.CurrencyCode}" + " " + $"{((viewModel.DPP + viewModel.VatValue)- viewModel.IncomeTaxValue).ToString("N", new CultureInfo("id-ID"))}", normal_font);
+
             tableNote.AddCell(cellLeftNoBorder);
 
             PdfPCell cellNote = new PdfPCell(tableNote); // dont remove
