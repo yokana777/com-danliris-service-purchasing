765ae0a575dfcb4e3326f52d8184421b6e5a9246
ryan <ryan.santosa@moonlay.com>
2021-03-22 21:21:25 +0700

    fixing feedback

--- Com.DanLiris.Service.Purchasing.Lib/PDFTemplates/GarmentPurchasingPDFTemplate.cs
+++ Com.DanLiris.Service.Purchasing.Lib/PDFTemplates/GarmentPurchasingPDFTemplate.cs
@@ -466,7 +466,7 @@ namespace Com.DanLiris.Service.Purchasing.Lib.PDFTemplates
             cellLeftNoBorder.Phrase = new Phrase(":", normal_font);
             tableNote.AddCell(cellLeftNoBorder);
             //cellLeftNoBorder.Phrase = new Phrase($"{viewModel.CurrencyCode}" + " " + $"{(totalPurchase + ppnPurchase).ToString("N", new CultureInfo("id-ID"))}", normal_font);
-            cellLeftNoBorder.Phrase = new Phrase($"{viewModel.CurrencyCode}" + " " + $"{((viewModel.DPP + viewModel.VatValue)- viewModel.IncomeTaxValue).ToString("N", new CultureInfo("id-ID"))}", normal_font);
+            cellLeftNoBorder.Phrase = new Phrase($"{viewModel.CurrencyCode}" + " " + $"{((viewModel.DPP + viewModel.VatValue)- viewModel.incom).ToString("N", new CultureInfo("id-ID"))}", normal_font);
 
             tableNote.AddCell(cellLeftNoBorder);
 
@@ -535,7 +535,7 @@ namespace Com.DanLiris.Service.Purchasing.Lib.PDFTemplates
             tableSignature.AddCell(cellSignatureContent);
             cellSignatureContent.Phrase = new Phrase("\n\n\n\n\n\n\n(   Kabag Pembelian    )", bold_font3);
             tableSignature.AddCell(cellSignatureContent);
-            cellSignatureContent.Phrase = new Phrase("\n\n\n\n\n\n\n(   Kabag Pembelian    )", bold_font3);
+            cellSignatureContent.Phrase = new Phrase("\n\n\n\n\n\n\n(   Kasie Pembelian    )", bold_font3);
             tableSignature.AddCell(cellSignatureContent);
             cellSignatureContent.Phrase = new Phrase("\n\n\n\n\n\n\n(     "+userName+"     )", bold_font3);
             tableSignature.AddCell(cellSignatureContent);
