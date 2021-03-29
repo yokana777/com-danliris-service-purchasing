1977f31ece2b4ce371f010efd8b356f6e65ce1d8
ryan <ryan.santosa@moonlay.com>
2021-03-29 22:35:47 +0700

    fixing mappign category

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20210329143629_FlagIsPaymentPaidAddingInDisposition.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20210329143629_FlagIsPaymentPaidAddingInDisposition.Designer.cs
@@ -13,9 +13,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210329143629_FlagIsPaymentPaidAddingInDisposition")]
+    partial class FlagIsPaymentPaidAddingInDisposition
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
