5079e940504a7f86f559d9f11bc9a7ec1bfba835
ryan <ryan.santosa@moonlay.com>
2021-03-29 08:19:49 +0700

    Get DOcument No by Date

--- Com.DanLiris.Service.Purchasing.Lib/Interfaces/IBankDocumentNumberGenerator.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Interfaces/IBankDocumentNumberGenerator.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace Com.DanLiris.Service.Purchasing.Lib.Interfaces
 {
     public interface IBankDocumentNumberGenerator
     {
         Task<string> GenerateDocumentNumber(string Type, string BankCode, string Username);
+        Task<string> GenerateDocumentNumber(string Type, string BankCode, string Username,DateTime Date);
     }
 }
