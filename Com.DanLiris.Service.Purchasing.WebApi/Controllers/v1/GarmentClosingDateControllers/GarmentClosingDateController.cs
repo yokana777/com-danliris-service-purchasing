c114eb258cc2d8049057efeb8a9247d85bca29cb
achensetyawati <setyawati@moonlay.com>
2021-06-10 16:07:03 +0700

    Update GarmentClosingDateController.cs

--- Com.DanLiris.Service.Purchasing.WebApi/Controllers/v1/GarmentClosingDateControllers/GarmentClosingDateController.cs
+++ Com.DanLiris.Service.Purchasing.WebApi/Controllers/v1/GarmentClosingDateControllers/GarmentClosingDateController.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using Com.DanLiris.Service.Purchasing.Lib.Interfaces;
+using Com.DanLiris.Service.Purchasing.Lib.Models.GarmentClosingDateModels;
 using Com.DanLiris.Service.Purchasing.Lib.Services;
+using Com.DanLiris.Service.Purchasing.Lib.ViewModels.GarmentClosingDateViewModels;
 using Com.DanLiris.Service.Purchasing.WebApi.Helpers;
+using Com.Moonlay.NetCore.Lib.Service;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -54,6 +57,43 @@ namespace Com.DanLiris.Service.Purchasing.WebApi.Controllers.v1.GarmentClosingDa
                     new ResultFormatter(ApiVersion, General.OK_STATUS_CODE, General.OK_MESSAGE)
                     .Ok(Data.Item1, info);
                 return Ok(Result);
+            }
+            catch (Exception e)
+            {
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.INTERNAL_ERROR_STATUS_CODE, e.Message)
+                    .Fail();
+                return StatusCode(General.INTERNAL_ERROR_STATUS_CODE, Result);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]GarmentClosingDateViewModel viewModel)
+        {
+            try
+            {
+                identityService.Username = User.Claims.Single(p => p.Type.Equals("username")).Value;
+                identityService.TimezoneOffset = int.Parse(Request.Headers["x-timezone-offset"].First());
+                identityService.Token = Request.Headers["Authorization"].First().Replace("Bearer ", "");
+
+                IValidateService validateService = (IValidateService)serviceProvider.GetService(typeof(IValidateService));
+                validateService.Validate(viewModel);
+
+                var Model = mapper.Map<GarmentClosingDate>(viewModel);
+
+                await facade.Create(Model, identityService.Username, identityService.TimezoneOffset);
+
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.CREATED_STATUS_CODE, General.OK_MESSAGE)
+                    .Ok();
+                return Created(String.Concat(Request.Path, "/", 0), Result);
+            }
+            catch (ServiceValidationExeption e)
+            {
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.BAD_REQUEST_STATUS_CODE, General.BAD_REQUEST_MESSAGE)
+                    .Fail(e);
+                return BadRequest(Result);
             }
             catch (Exception e)
             {
