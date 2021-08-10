69cdee6eb17ffb37d6612b9e481818479bd3e52d
ichwankm <ichwankm@gmail.com>
2021-08-10 09:07:25 +0700

    complete merge

--- Com.DanLiris.Service.Purchasing.Test/Facades/GarmentExternalPurchaseOrderTests/BasicTest.cs
+++ Com.DanLiris.Service.Purchasing.Test/Facades/GarmentExternalPurchaseOrderTests/BasicTest.cs
@@ -714,5 +714,14 @@ namespace Com.DanLiris.Service.Purchasing.Test.Facades.GarmentExternalPurchaseOr
             var Response = facade.ReadItemForUnitDOByRO(ro, Filter: "{'RONo':'" + ro + "'}");
             Assert.NotNull(Response);
         }
+
+        [Fact]
+        public void Should_Success_Get_IsUnpost()
+        {
+            GarmentExternalPurchaseOrderFacade facade = new GarmentExternalPurchaseOrderFacade(GetServiceProvider().Object, _dbContext(GetCurrentMethod()));
+            var result = facade.GetIsUnpost(It.IsAny<int>());
+
+            Assert.False(result);
+        }
     }
 }
