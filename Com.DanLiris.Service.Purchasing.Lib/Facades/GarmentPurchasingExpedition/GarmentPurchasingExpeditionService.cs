fb60d1e3bcd626f1183a8d1ff87b64a2737f2dad
ichwankm <ichwankm@gmail.com>
2021-09-30 09:17:32 +0700

    Bug Fix Koreksi Ekspedisi NI

--- Com.DanLiris.Service.Purchasing.Lib/Facades/GarmentPurchasingExpedition/GarmentPurchasingExpeditionService.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Facades/GarmentPurchasingExpedition/GarmentPurchasingExpeditionService.cs
@@ -124,11 +124,26 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentPurchasingExpeditio
                     else
                         total = (double)element.TotalCorrection;
 
-                    if (element.UseVat)
-                        total += total * 0.1;
+                    return total;
+                });
 
-                    if (element.UseIncomeTax)
-                        total -= total * (double)(element.IncomeTaxRate / 100);
+                var totalCorrection = selectedCorrections.Sum(element =>
+                {
+                    var selectedCorrectionItems = correctionItems.Where(item => item.GCorrectionId == element.Id);
+                    var garmentInvoicesIds = internalNoteItems.Where(item => item.Id == internalNoteDetails.Where(i => i.DOId == element.DOId).Select(x => x.GarmentItemINId).FirstOrDefault()).Select(entity => entity.InvoiceId);
+                    var garmentInvoicesCorrection = invoices.Where(item => garmentInvoicesIds.Contains(item.Id)).FirstOrDefault();
+
+                    var total = element.CorrectionType.ToUpper() == "RETUR" ? (double)selectedCorrectionItems.Sum(item => item.PricePerDealUnitAfter * item.Quantity) : (double)element.TotalCorrection;
+
+                    if (garmentInvoicesCorrection.UseVat && garmentInvoicesCorrection.IsPayVat)
+                    {
+                        total += element.CorrectionType.ToUpper() == "RETUR" ? (double)selectedCorrectionItems.Sum(item => item.PricePerDealUnitAfter * item.Quantity) * 0.1 : (double)element.TotalCorrection * 0.1;
+                    }
+
+                    if (garmentInvoicesCorrection.UseIncomeTax && garmentInvoicesCorrection.IsPayTax)
+                    {
+                        total -= element.CorrectionType.ToUpper() == "RETUR" ? (double)selectedCorrectionItems.Sum(item => item.PricePerDealUnitAfter * item.Quantity) * (double)(element.IncomeTaxRate / 100) : (double)element.TotalCorrection * (double)(element.IncomeTaxRate / 100);
+                    }
 
                     return total;
                 });
@@ -138,14 +153,18 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentPurchasingExpeditio
                     var total = element.TotalAmount;
 
                     if (element.UseVat && element.IsPayVat)
+                    {
                         total += element.TotalAmount * 0.1;
-
+                    }
+                    
                     if (element.UseIncomeTax && element.IsPayTax)
+                    {
                         total -= element.TotalAmount * (element.IncomeTaxRate / 100);
-
+                    }
+                    
                     return total;
                 });
-                totalAmount += correctionAmount;
+                totalAmount += totalCorrection;
 
                 var correctionVat = selectedCorrections.Sum(element =>
                 {
@@ -154,8 +173,10 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentPurchasingExpeditio
                     var total = 0.0;
 
                     if (element.UseVat)
+                    {
                         total += element.CorrectionType.ToUpper() == "RETUR" ? (double)selectedCorrectionItems.Sum(item => item.PricePerDealUnitAfter * item.Quantity) * 0.1 : (double)element.TotalCorrection * 0.1;
-
+                    }
+                    
                     return total;
                 });
 
@@ -164,12 +185,18 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentPurchasingExpeditio
                     var vat = 0.0;
 
                     if (element.UseVat && element.IsPayVat)
+                    {
                         vat += element.TotalAmount * 0.1;
-
+                    }
+                    
                     return vat;
                 });
-                vatTotal += correctionVat;
 
+                if (vatTotal != 0)
+                {
+                    vatTotal += correctionVat;
+                }
+                
                 var correctionIncomeTax = selectedCorrections.Sum(element =>
                 {
                     var selectedCorrectionItems = correctionItems.Where(item => item.GCorrectionId == element.Id);
@@ -177,7 +204,9 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentPurchasingExpeditio
                     var total = 0.0;
 
                     if (element.UseIncomeTax)
+                    {
                         total += element.CorrectionType.ToUpper() == "RETUR" ? (double)selectedCorrectionItems.Sum(item => item.PricePerDealUnitAfter * item.Quantity) * (double)(element.IncomeTaxRate / 100) : (double)element.TotalCorrection * (double)(element.IncomeTaxRate / 100);
+                    }
 
                     return total;
                 });
@@ -187,12 +216,18 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentPurchasingExpeditio
                     var incomeTax = 0.0;
 
                     if (element.UseIncomeTax && element.IsPayTax)
+                    {
                         incomeTax += element.TotalAmount * (element.IncomeTaxRate / 100);
+                    }
 
                     return incomeTax;
                 });
-                incomeTaxTotal += correctionIncomeTax;
 
+                if (incomeTaxTotal != 0)
+                {
+                    incomeTaxTotal += correctionIncomeTax;
+                }
+                
                 //var productInvoice = invoices.Where(element => selectedInvoiceIds.Contains(element.Id)).Select(element =>
                 //{
                 //    var firstProduct = element.entity.Items;
