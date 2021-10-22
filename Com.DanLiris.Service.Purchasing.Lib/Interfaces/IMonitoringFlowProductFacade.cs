3045f711f59432c31673d7949d7a29c4ae6ef066
putrapradesa <49633721+putrapradesa@users.noreply.github.com>
2021-10-22 09:39:47 +0700

    Add-monitoringflowproductreport

---
+++ Com.DanLiris.Service.Purchasing.Lib/Interfaces/IMonitoringFlowProductFacade.cs
@@ -0,0 +1,14 @@
+﻿using Com.DanLiris.Service.Purchasing.Lib.ViewModels.GarmentReports;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Com.DanLiris.Service.Purchasing.Lib.Interfaces
+{
+    public interface IMonitoringFlowProductFacade
+    {
+        Tuple<List<MonitoringFlowProductViewModel>, int> GetFlow(string Dono, string beacukaiNo, string ProductCode);
+        MemoryStream GetProductFlowExcel(string Dono, string beacukaiNo, string ProductCode);
+    }
+}
