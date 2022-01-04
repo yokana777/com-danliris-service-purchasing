0e298a1a8455febf1a17aff01874cb4cfb9fb706
achensetyawati <setyawati@moonlay.com>
2022-01-04 13:59:50 +0700

    Update GarmentUnitExpenditureNoteFacade.cs

--- Com.DanLiris.Service.Purchasing.Lib/Facades/GarmentUnitExpenditureNoteFacade/GarmentUnitExpenditureNoteFacade.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Facades/GarmentUnitExpenditureNoteFacade/GarmentUnitExpenditureNoteFacade.cs
@@ -1086,7 +1086,7 @@ namespace Com.DanLiris.Service.Purchasing.Lib.Facades.GarmentUnitExpenditureNote
                     }
 
 
-                    if (garmentUnitExpenditureNote.ExpenditureType == "TRANSFER")
+                    if (garmentUnitExpenditureNote.ExpenditureType == "TRANSFER" || (garmentUnitExpenditureNote.ExpenditureType == "SAMPLE" && garmentUnitExpenditureNote.UnitSenderCode != "SMP1"))
                     {
                         var garmentInventoryDocumentIn = GenerateGarmentInventoryDocument(oldGarmentUnitExpenditureNote, "IN");
                         dbSetGarmentInventoryDocument.Add(garmentInventoryDocumentIn);
