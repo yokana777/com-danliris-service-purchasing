9fbb9351e506bdeb2db7d1d5e7c64f0cd41e5606
yokana666 <kawaiirockmansick@gmail.com>
2022-03-16 10:50:07 +0700

    update-garment-external-purchase-order-vat
    16032022

--- Com.DanLiris.Service.Purchasing.Lib/Migrations/20220316020500_change_column_name_vat_external_purchase_order.Designer.cs
+++ Com.DanLiris.Service.Purchasing.Lib/Migrations/20220316020500_change_column_name_vat_external_purchase_order.Designer.cs
@@ -13,9 +13,10 @@ using System;
 namespace Com.DanLiris.Service.Purchasing.Lib.Migrations
 {
     [DbContext(typeof(PurchasingDbContext))]
-    partial class PurchasingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220316020500_change_column_name_vat_external_purchase_order")]
+    partial class change_column_name_vat_external_purchase_order
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
